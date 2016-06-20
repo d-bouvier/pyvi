4d9f5332e8718e112f76080f07b5f3bd353e09a1
Damien Bouvier <damien.bouvier@ircam.fr>
2016-06-20 15:08:44 +0200

    Creates simulation module
    This module will regroup all functions necessary for numerical simulation of nonlinear systems
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

---
+++ simulation.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Set of functions for the numerical simulation of a nonlinear systems given
+its state-space representation.
+
+@author:    bouvier@ircam.fr
+            Damien Bouvier, IRCAM, Paris
+
+Developed for Python 3.5.1
+"""
+
+#==============================================================================
+# Importations
+#==============================================================================
+
+import numpy as np
+from scipy  import fftpack
+from matplotlib import pyplot as plt
+
+
+#==============================================================================
+# Main script
+#==============================================================================
+
+if __name__ == '__main__':
+    """
+    Main script for testing
+    """
+    print('Hello world\n')
+
+
+#==============================================================================
+# Functions
+#==============================================================================
