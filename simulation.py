d92beef28df24894551fcd80eb3fdd2592c44415
Damien Bouvier <damien.bouvier@ircam.fr>
2016-06-21 18:27:39 +0200

    Add the simulation for the linear part
    List of changes:
    	- variable name all ***_size to ***_dim
    	- shape of signal arrays from (time, dimension) to (dimension, time)
    	- add a condition for enforcing god shape of input
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- simulation.py
+++ simulation.py
@@ -35,9 +35,9 @@ def simulation(input_sig, matrices,
     C_m = matrices[2]
     D_m = matrices[3]
 
-    input_size = sizes[0]
-    state_size = sizes[1]
-    output_size = sizes[2]
+    input_dim = sizes[0]
+    state_dim = sizes[1]
+    output_dim = sizes[2]
 
     h_mpq_bool = m_pq[0]
     h_mpq = m_pq[1]
@@ -48,22 +48,30 @@ def simulation(input_sig, matrices,
     sig_len = np.shape(input_sig)[0]
     sampling_time = 1/fs
     w_filter = linalg.expm(A_m*sampling_time)
-    sampling_bias = np.dot(np.linalg.inv(A_m),
-                           w_filter - np.identity(state_size))
-
+    holder_bias_state = np.dot(np.linalg.inv(A_m),
+                               w_filter - np.identity(state_dim))
+    holder_bias_input = np.dot(holder_bias_state, B_m)
+    
     # State and output initialization
-    state_sig = np.empty((sig_len, state_size))
-    state_sig[0,] = 0
-    state_by_order = np.empty((nl_order_max, sig_len, state_size))
-    state_by_order[0,] = 0
-    output_sig = np.empty((sig_len, output_size))
-    output_sig[0,] = 0
-    
-    
+    state_sig = np.zeros((state_dim, sig_len))
+    state_by_order = np.zeros((nl_order_max, state_dim, sig_len))
+    output_sig = np.zeros((output_dim, sig_len))
+
+    # Enforce good shape when dimension is 1
+    if input_dim == 1:
+        input_sig.shape = (input_dim, sig_len)
+        holder_bias_input.shape = (state_dim, input_dim)
+
     ## Numerical simulation ##
     # Main loop (on time indexes)
+    for k in np.arange(sig_len-1):
+        state_by_order[0,:,k+1] = \
+                    np.dot(w_filter, state_by_order[0,:,k]) +\
+                    np.dot(holder_bias_input, input_sig[:,k])
     
-    return output_sig
+    state_sig = state_by_order.sum(0)
+    output_sig = np.dot(C_m, state_sig) + np.dot(D_m, input_sig)
+    return output_sig.transpose()
 
     
 def hp_parameters():
@@ -72,9 +80,9 @@ def hp_parameters():
     representation for the loudspeaker SICA Z000900 
     """ 
 
-    state_size = 3
-    input_size = 1
-    output_size = 1
+    state_dim = 3
+    input_dim = 1
+    output_dim = 1
     sym_bool = True
     
     ## Linear part ##
@@ -95,7 +103,7 @@ def hp_parameters():
                     [Bl/Mms, -k/Mms, -Rms/Mms]]) # State-to-state matrix
     B_m = np.array([1/Le, 0, 0]); # Input-to-state matrix
     C_m = np.array([[1, 0, 0]]) # State-to-output matrix  
-    D_m = np.zeros((output_size, input_size)) # Input-to-output matrix    
+    D_m = np.zeros((output_dim, input_dim)) # Input-to-output matrix    
 
     ## Nonlinear part ##
     
@@ -106,15 +114,15 @@ def hp_parameters():
     h_mpq_bool = (lambda p, q: q==0) 
     h_npq_bool = (lambda p, q: False)
     # Handles for fonction giving the Mpq tensor
-    h_mpq = (lambda p, q: hp_mpq_tensor(p, q, state_size,  mpq_coeff(p)))
+    h_mpq = (lambda p, q: hp_mpq_tensor(p, q, state_dim,  mpq_coeff(p)))
     h_npq = (lambda p, q: None)
 
-    def hp_mpq_tensor(p, q, state_size, coeff_value):
+    def hp_mpq_tensor(p, q, state_dim, coeff_value):
         """
         Gives the tensor form of the Mpq function (with q = 0)
         """ 
         if q==0:
-            Mpq_tensor = np.zeros((state_size,)*(p+1))
+            Mpq_tensor = np.zeros((state_dim,)*(p+1))
             idx = np.concatenate((np.array([2], dtype=int), np.ones(p, dtype=int)))
             Mpq_tensor[tuple(idx)] = coeff_value
             return Mpq_tensor
@@ -122,7 +130,7 @@ def hp_parameters():
             return None
             
     return (A_m, B_m, C_m, D_m), (h_mpq_bool, h_mpq), (h_npq_bool, h_npq), \
-    (input_size, state_size, output_size), sym_bool
+    (input_dim, state_dim, output_dim), sym_bool
 
 
 #==============================================================================
@@ -139,8 +147,8 @@ if __name__ == '__main__':
     # Input signal
     fs = 44100 # Sampling frequency
     T = 2 # Duration 
-    f1 = 220 # Starting fundamental frequency
-    f2 = 4400 # Ending fundamental frequency
+    f1 = 400 # Starting fundamental frequency
+    f2 = 50 # Ending fundamental frequency
     A = 1 # Amplitude    
     
     time_vector = np.arange(0, T, step=1/fs)
@@ -149,7 +157,7 @@ if __name__ == '__main__':
     
     plt.figure(0)
     plt.plot(time_vector, sig)
-    plt.xlim([0, 0.025])
+    plt.xlim([0, 0.05])
     plt.show
 
     # Loudspeakers parameters
@@ -160,5 +168,5 @@ if __name__ == '__main__':
     
     plt.figure(1)
     plt.plot(time_vector, output)
-    plt.xlim([0, 0.025])
+    plt.xlim([0, 0.05])
     plt.show
