c41f22339fbe3cd27d3cdfe1178425c82842cb0a
Damien Bouvier <damien.bouvier@ircam.fr>
2016-06-21 14:34:23 +0200

    Add hp_parameters function that creates parameters for simulating the SICA Z000900
    This function returns the following:
    	- the 4 matrices A, B, C and D from linear state-space representations
    	- the 4 following lambda function:
    		- h_mpq_bool that says if the M(p,q) function is used in the dynamic equation
    		- h_mpq that gives the corresponding Mpq tensor
    		- h_npq_bool that says if the N(p,q) function is used in the output equation
    		- h_npq that gives the corresponding Mpq tensor
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- simulation.py
+++ simulation.py
@@ -18,6 +18,69 @@ from scipy  import fftpack
 from matplotlib import pyplot as plt
 
 
+    
+#==============================================================================
+# Functions
+#==============================================================================
+
+def hp_parameters():
+    """
+    Gives the linear matrices and nonlinear operators of the state-space
+    representation for the loudspeaker SICA Z000900 
+    """ 
+
+    state_order = 3
+    input_order = 1
+    output_order = 1
+    boolsym_bool = True
+    
+    ## Linear part ##
+
+    # Electric parameters
+    Bl = 2.99 # Electodynamic driving parameter [T.m]
+    Re = 5.7 # Electrical resistance of voice coil   [Ohm]
+    Le  =   0.11e-3 # Coil inductance [H]
+    # Mechanical parameters
+    Mms = 1.9e-3; # Mechanical mass [kg]
+    Cms = 544e-6; # Mechanical compliance [m.N-1]
+    Qms = 4.6;
+    k = 1 / Cms # Suspension stiffness [N.m-1]
+    Rms = np.sqrt(k * Mms)/Qms; # Mechanical damping and drag force [kg.s-1]   
+    # State-space matrices
+    A_m = np.array([[-Re/Le, 0, -Bl/Le],
+                    [0, 0, 1],
+                    [Bl/Mms, -k/Mms, -Rms/Mms]]) # State-to-state matrix
+    B_m = np.array([1/Le, 0, 0]); # Input-to-state matrix
+    C_m = np.array([[1, 0, 0]]) # State-to-output matrix  
+    D_m = np.zeros((output_order, input_order)) # Input-to-output matrix    
+
+    ## Nonlinear part ##
+    
+    # Suspension stiffness polynomial expansion
+    k_poly_coeff_v = np.array([k, -554420.0, 989026000])
+    mpq_coeff = k_poly_coeff_v/Mms
+    # Handles for fonction saying if Mpq and Npq functions are used
+    h_mpq_bool = (lambda p, q: q==0) 
+    h_npq_bool = (lambda p, q: False)
+    # Handles for fonction giving the Mpq tensor
+    h_mpq = (lambda p, q: hp_mpq_tensor(p, q, state_order,  mpq_coeff(p)))
+    h_npq = (lambda p, q: None)
+
+    def hp_mpq_tensor(p, q, state_order, coeff_value):
+        """
+        Gives the tensor form of the Mpq function (with q = 0)
+        """ 
+        if q==0:
+            Mpq_tensor = np.zeros((state_order,)*(p+1))
+            idx = np.concatenate((np.array([2], dtype=int), np.ones(p, dtype=int)))
+            Mpq_tensor[tuple(idx)] = coeff_value
+            return Mpq_tensor
+        else:
+            return None
+            
+    return A_m, B_m, C_m, D_m, h_mpq_bool, h_mpq, h_npq_bool, h_npq
+
+
 #==============================================================================
 # Main script
 #==============================================================================
@@ -43,7 +106,6 @@ if __name__ == '__main__':
     plt.xlim([0, 0.025])
     plt.show
 
-
-#==============================================================================
-# Functions
-#==============================================================================
+    # Loudspeakers parameters
+    A_m, B_m, C_m, D_m, h_mpq_bool, h_mpq, h_npq_bool, h_npq = hp_parameters()
+    print(A_m, B_m, C_m, D_m, h_mpq_bool, h_mpq, h_npq_bool, h_npq, sep='\n')
