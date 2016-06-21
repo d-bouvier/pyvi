e7437337cb62910634f3d2c1a6f94549d48bd83e
Damien Bouvier <damien.bouvier@ircam.fr>
2016-06-21 17:39:33 +0200

    Add the simulation function (not finished)
    This commit adds:
    	- changes on output for hp_parameters (variable grouped in tuple)
    	- call to simulation
    	- plot of output signal of simulation
    	- changed names ***_order to ***_size
    	- In simulation:
     		- unpacking of parameters from tuple
    		- computing of the filter matrix and bias matrix for discretization
    		- signal initialization
    		- return of the output signal
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- simulation.py
+++ simulation.py
@@ -14,25 +14,68 @@ Developed for Python 3.5.1
 #==============================================================================
 
 import numpy as np
-from scipy  import fftpack
+from scipy  import fftpack, linalg
 from matplotlib import pyplot as plt
 
 
-    
 #==============================================================================
 # Functions
 #==============================================================================
 
+def simulation(input_sig, matrices,
+               m_pq=(lambda p,q: False, lambda p,q: None),
+               n_pq=(lambda p,q: False, lambda p,q: None),
+               sizes=(1, 1, 1), sym_bool=True,
+               fs=44100, nl_order_max=1):
+
+    ## Init ##
+    # Unpack values
+    A_m = matrices[0]
+    B_m = matrices[1]
+    C_m = matrices[2]
+    D_m = matrices[3]
+
+    input_size = sizes[0]
+    state_size = sizes[1]
+    output_size = sizes[2]
+
+    h_mpq_bool = m_pq[0]
+    h_mpq = m_pq[1]
+    h_npq_bool = n_pq[0]
+    h_npq = n_pq[1]
+    
+    # Compute parameters
+    sig_len = np.shape(input_sig)[0]
+    sampling_time = 1/fs
+    w_filter = linalg.expm(A_m*sampling_time)
+    sampling_bias = np.dot(np.linalg.inv(A_m),
+                           w_filter - np.identity(state_size))
+
+    # State and output initialization
+    state_sig = np.empty((sig_len, state_size))
+    state_sig[0,] = 0
+    state_by_order = np.empty((nl_order_max, sig_len, state_size))
+    state_by_order[0,] = 0
+    output_sig = np.empty((sig_len, output_size))
+    output_sig[0,] = 0
+    
+    
+    ## Numerical simulation ##
+    # Main loop (on time indexes)
+    
+    return output_sig
+
+    
 def hp_parameters():
     """
     Gives the linear matrices and nonlinear operators of the state-space
     representation for the loudspeaker SICA Z000900 
     """ 
 
-    state_order = 3
-    input_order = 1
-    output_order = 1
-    boolsym_bool = True
+    state_size = 3
+    input_size = 1
+    output_size = 1
+    sym_bool = True
     
     ## Linear part ##
 
@@ -52,7 +95,7 @@ def hp_parameters():
                     [Bl/Mms, -k/Mms, -Rms/Mms]]) # State-to-state matrix
     B_m = np.array([1/Le, 0, 0]); # Input-to-state matrix
     C_m = np.array([[1, 0, 0]]) # State-to-output matrix  
-    D_m = np.zeros((output_order, input_order)) # Input-to-output matrix    
+    D_m = np.zeros((output_size, input_size)) # Input-to-output matrix    
 
     ## Nonlinear part ##
     
@@ -63,22 +106,23 @@ def hp_parameters():
     h_mpq_bool = (lambda p, q: q==0) 
     h_npq_bool = (lambda p, q: False)
     # Handles for fonction giving the Mpq tensor
-    h_mpq = (lambda p, q: hp_mpq_tensor(p, q, state_order,  mpq_coeff(p)))
+    h_mpq = (lambda p, q: hp_mpq_tensor(p, q, state_size,  mpq_coeff(p)))
     h_npq = (lambda p, q: None)
 
-    def hp_mpq_tensor(p, q, state_order, coeff_value):
+    def hp_mpq_tensor(p, q, state_size, coeff_value):
         """
         Gives the tensor form of the Mpq function (with q = 0)
         """ 
         if q==0:
-            Mpq_tensor = np.zeros((state_order,)*(p+1))
+            Mpq_tensor = np.zeros((state_size,)*(p+1))
             idx = np.concatenate((np.array([2], dtype=int), np.ones(p, dtype=int)))
             Mpq_tensor[tuple(idx)] = coeff_value
             return Mpq_tensor
         else:
             return None
             
-    return A_m, B_m, C_m, D_m, h_mpq_bool, h_mpq, h_npq_bool, h_npq
+    return (A_m, B_m, C_m, D_m), (h_mpq_bool, h_mpq), (h_npq_bool, h_npq), \
+    (input_size, state_size, output_size), sym_bool
 
 
 #==============================================================================
@@ -89,6 +133,8 @@ if __name__ == '__main__':
     """
     Main script for testing
     """
+    
+    plt.close("all")
     
     # Input signal
     fs = 44100 # Sampling frequency
@@ -101,11 +147,18 @@ if __name__ == '__main__':
     f0_vector = np.linspace(f1, f2, num=len(time_vector))
     sig = np.cos(2 * np.pi * f0_vector * time_vector)
     
-    plt.figure
+    plt.figure(0)
     plt.plot(time_vector, sig)
     plt.xlim([0, 0.025])
     plt.show
 
     # Loudspeakers parameters
-    A_m, B_m, C_m, D_m, h_mpq_bool, h_mpq, h_npq_bool, h_npq = hp_parameters()
-    print(A_m, B_m, C_m, D_m, h_mpq_bool, h_mpq, h_npq_bool, h_npq, sep='\n')
+    matrices, m_pq, n_pq, sizes, sym_bool = hp_parameters()
+    
+    # Simulation
+    output = simulation(sig, matrices, m_pq, n_pq, sizes, sym_bool, fs)
+    
+    plt.figure(1)
+    plt.plot(time_vector, output)
+    plt.xlim([0, 0.025])
+    plt.show
