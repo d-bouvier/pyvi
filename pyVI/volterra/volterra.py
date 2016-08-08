7345c1ad2a9470c494aba9bd748dd4bc923acbe1
Damien Bouvier <damien.bouvier@ircam.fr>
2016-08-08 14:32:02 +0200

    Add a Volterra class for volterra series
    Adds a Volterra class with its __init__, __repr__ and __print__ methods.
    Changes the Kernel.__init__ method and debug Kernel.__str__ (previously called __print__)
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyVI/volterra/volterra.py
+++ pyVI/volterra/volterra.py
@@ -5,7 +5,7 @@ Description
 @author: bouvier (bouvier@ircam.fr)
          Damien Bouvier, IRCAM, Paris
 
-Created on Mon Aug  8 10:22:17 2016
+Created on Mon Aug  8 12:10:11 2016
 Developed for Python 3.5.1
 """
 
@@ -15,49 +15,53 @@ Developed for Python 3.5.1
 
 import sympy as sp
 import numpy as np
+from kernel import Kernel
 from abc import abstractmethod
 
 #==============================================================================
 # Class
 #==============================================================================
 
-class Kernel:
-    """ Class that defines a Volterra kernel described by:
+class Volterra:
+    """ Class that defines Volterra serie of a system, described by:
     - its expression
     - its order of nonlinearity
     Also defines if it's a symmetric kernel and the list of sympy symbols used 
     in the expression."""
     
-    def __init__(self, expr=sp.Integer(1), order=1, **kwargs):
-        self.expr = expr
-        self.order = order
-
+    def __init__(self, kernels=[Kernel()], **kwargs):
+        self.kernels = kernels
+        self.list_kernels = []
+        for idx, kernel in enumerate(self.kernels):
+            self.list_kernels.append(kernel.order)
+                
+        if 'order_max' in kwargs:
+            self.order_max = kwargs['order_max']
+        else:
+            self.order_max = max(self.list_kernels)    
         if 'symmetric' in kwargs:
             self.symmetric = kwargs['symmetric']
         else:
-            self.symmetric = None
+            self.symmetric = True
+            for idx, kernel in enumerate(self.kernels):
+                self.symmetric = self.symmetric and kernel.symmetric
         if 'symbols' in kwargs:
             self.symbols = kwargs['symbols']
         else:
-            self.symbols = sp.var('H{} s(1:{})'.format(self.order,
-                                                       self.order + 1))
+            self.symbols = sp.var('H s(1:{})'.format(self.order_max + 1))
       
     
     def __repr__(self):
-        repr_str = sp.pretty(self.symbols[0])        
-        repr_str += sp.pretty(self.symbols[1:])
-        repr_str += ' = '
-        repr_str += sp.pretty(self.expr)
+        repr_str = ''
+        for idx, kernel in enumerate(self.kernels):
+            repr_str += kernel.__repr__()
+            repr_str += '\n'
         return repr_str
-        
+    
     def __str__(self):
-        repr_str = 'Volterra kernel of order {}:\n'.format(self.order)
+        repr_str = 'Volterra serie up to order {}:\n'.format(self.order_max)
         repr_str += self.__repr__()
         return repr_str
-
-    @abstractmethod
-    def plot(self):
-        return NotImplementedError
 
     @abstractmethod
     def symmetrize(self):
@@ -74,4 +78,3 @@ class Kernel:
 #==============================================================================
 # Functions
 #==============================================================================
-
