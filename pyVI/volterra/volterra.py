ab664ee76aca887b28726f173575cc08d02ba603
Damien Bouvier <damien.bouvier@ircam.fr>
2016-08-08 15:00:35 +0200

    Debug last commit
    Put definition of Kernel class before Volterra class
    Suppress import of kernel (irrelevant and useless now)
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyVI/volterra/volterra.py
+++ pyVI/volterra/volterra.py
@@ -14,12 +14,62 @@ Developed for Python 3.5.1
 #==============================================================================
 
 import sympy as sp
-from kernel import Kernel
 from abc import abstractmethod
 
 #==============================================================================
 # Class
 #==============================================================================
+
+class Kernel:
+    """ Class that defines a Volterra kernel described by:
+    - its expression
+    - its order of nonlinearity
+    Also defines if it's a symmetric kernel and the list of sympy symbols used 
+    in the expression."""
+    
+    def __init__(self, expr=sp.Integer(1), order=1, **kwargs):
+        self.expr = expr
+        self.order = order
+
+        if 'symmetric' in kwargs:
+            self.symmetric = kwargs['symmetric']
+        else:
+            self.symmetric = None
+        if 'symbols' in kwargs:
+            self.symbols = kwargs['symbols']
+        else:
+            self.symbols = sp.var('H{} s(1:{})'.format(self.order,
+                                                       self.order + 1))
+      
+    
+    def __repr__(self):
+        repr_str = sp.pretty(self.symbols[0])        
+        repr_str += sp.pretty(self.symbols[1:])
+        repr_str += ' = '
+        repr_str += sp.pretty(self.expr)
+        return repr_str
+        
+    def __str__(self):
+        repr_str = 'Volterra kernel of order {}:\n'.format(self.order)
+        repr_str += self.__repr__()
+        return repr_str
+
+    @abstractmethod
+    def plot(self):
+        return NotImplementedError
+
+    @abstractmethod
+    def symmetrize(self):
+        return NotImplementedError
+
+    @abstractmethod
+    def regularize(self):
+        return NotImplementedError
+
+    @abstractmethod
+    def triangularize(self):
+        return NotImplementedError
+
 
 class Volterra:
     """ Class that defines Volterra serie of a system, described by:
@@ -65,9 +115,7 @@ class Volterra:
     @abstractmethod
     def symmetrize(self):
         for idx, kernel in enumerate(self.kernels):
-            print('a')
             self.list_kernels[idx] = kernel.symmetrize()
-            print('b')
 
     @abstractmethod
     def regularize(self):
@@ -86,7 +134,8 @@ class Volterra:
     @abstractmethod
     def composition(self, volterra):
         return NotImplementedError
-        
+
+
 #==============================================================================
 # Functions
 #==============================================================================
