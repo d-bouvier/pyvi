dc32e2522928dc607b94bf4a035c7b8aad7bf599
Damien Bouvier <damien.bouvier@ircam.fr>
2016-08-11 12:26:03 +0200

    Integrate StateSpace in System class
    Modifications:
    	- change the __init__(), can now take a string as label for the system
    	- __init__() now creates Symbol instance
    	- add a method for instantiating a StateSpace as a System attribute
    	- add a __repr__()
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyVI/system.py
+++ pyVI/system.py
@@ -36,9 +36,39 @@ class System:
     """
     
     
-    def __init__(self, Am, Bm, Cm, Dm, mpq_dict={}, npq_dict={}, **kwargs):
+    def __init__(self, label='Test', *args, **kwargs):
         """Initialize the system."""
+        self.label = label
+        self.dim_input = kwargs.get('dim_input', None)
+        self.dim_output = kwargs.get('dim_output', None)
+        
+        self._has_state_space_repr = False
+        self._has_volterra = False
+        
+        from symbols.symbols import Symbols
+        self.symbols = Symbols(3)
+    
+    
+    def add_state_space_repr(self, Am, Bm, Cm, Dm, mpq={}, npq={}):
+        from system.statespace import StateSpace
+        self.state_space = StateSpace(Am, Bm, Cm, Dm, mpq, npq)
 
+        self.dim_input = self.state_space.dim_input
+        self.dim_output = self.state_space.dim_output
+        self._has_state_space_repr = True
+        
+    
+    def __repr__(self):
+        repr_str = ''
+        if self._has_state_space_repr:
+            repr_str += 'State-space representation\n'
+            repr_str += self.state_space.__repr__()
+        if self._has_volterra:
+            repr_str += 'Volterra series\n'
+            repr_str += self.volterra.__repr__()
+        return repr_str
+    
+    
     @abstractmethod    
     def _is_linear(self):
         """Check if the system is linear."""
