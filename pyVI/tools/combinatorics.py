21e8711d6f862b89e096364a2008f045fef906e9
Damien Bouvier <damien.bouvier@ircam.fr>
2016-08-12 15:24:09 +0200

    Add multilinear_combinatorics() function in tools.combinatorics
    This function gives combinatorics for a given set of multilinear function.
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

---
+++ pyVI/tools/combinatorics.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Description
+
+@author: bouvier (bouvier@ircam.fr)
+         Damien Bouvier, IRCAM, Paris
+
+Created on Fri Aug 12 10:46:46 2016
+Developed for Python 3.5.1
+"""
+
+#==============================================================================
+# Importations
+#==============================================================================
+
+import itertools as itertbx
+
+
+#==============================================================================
+# Functions
+#==============================================================================
+
+def multilinear_combinatorics(used_pq, order_max=3):
+    """Returns combinatorics for a given set of multilinear functions.
+    
+    This function takes a set of (p, q) values (p and q being integers) and an
+    optionnal maximum order, and returns a dictionary 'list_orders' structured
+    as follows:
+    - 'list_orders' keys are nonlinear order, and the corresponging value is
+       a dictionnary 'pq_by_order';
+    - 'pq_by_order' keys are tuple (p, q), and the corresponding value is the
+       set of all possible combination for the order of the state-input of the
+       corresponding multilinear function."""
+       
+    dict_pq_by_order = dict()
+    dict_sets = dict()
+    list_orders = [1]
+    
+    # Compute, for each order of onlinearity, the set of (p, q) functions
+    for n in range(2, order_max+1):
+        # Take the (p, q) functions of previous order (if exists)
+        dict_pq_by_order[n] = dict_pq_by_order.get(n-1, set()).copy()
+        # Discard the (0, n-1) function for the order n
+        dict_pq_by_order[n].discard((0, n-1))
+        # Add the new (p, q) functions (such that p+q=n)
+        dict_pq_by_order[n].update({(n-q, q) for q in range(n+1)} & used_pq)
+        # If there is no (p, q) function at this order, discard the key 'n'
+        if len(dict_pq_by_order[n]) == 0:
+            del dict_pq_by_order[n]
+    
+    # Compute, for a given order and (p, q) function, the set of possible input
+    for n in sorted(dict_pq_by_order):
+        dict_sets[n] = dict()
+        print(list_orders)
+        # Loop on all (p, q) functions for this order
+        for (p, q) in dict_pq_by_order[n]:
+            dict_sets[n][(p, q)] = set()
+            # Loop on all permutations with repetitions of p elements taken
+            # from all inferior nonlinear orders
+            for possible_set in itertbx.product(list_orders, repeat=p):
+                # Discard all those not giving homogeneous order n
+                if sum(possible_set) == n-q:
+                    dict_sets[n][(p, q)].add(possible_set)
+            # Discard this (p, q) function if no possible inputs were found
+            if len(dict_sets[n][(p, q)]) == 0:
+                del dict_sets[n][(p, q)]
+        # Discard this order if no (p, q) function is used
+        if len(dict_sets[n]) == 0:
+            del dict_sets[n]
+        else:
+            list_orders.append(n)
+    
+    return dict_sets
+
