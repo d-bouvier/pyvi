bc398aca7e81d159c02bdc1b155a3f6d1e6309dd
Damien Bouvier <damien.bouvier@ircam.fr>
2016-09-15 11:44:31 +0200

    Suppress main fct and clarify header docstring
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- simulation.py
+++ simulation.py
@@ -6,6 +6,7 @@ its state-space representation.
 @author:    bouvier@ircam.fr
             Damien Bouvier, IRCAM, Paris
 
+Last modified on 15 Sept. 2016
 Developed for Python 3.5.1
 """
 
@@ -14,7 +15,7 @@ Developed for Python 3.5.1
 #==============================================================================
 
 import numpy as np
-from scipy import fftpack, linalg
+from scipy import linalg
 from matplotlib import pyplot as plt
 
 
@@ -314,109 +315,3 @@ def simulation(input_sig, matrices,
     # Function outputs
     input_sig.shape = (sig_len, input_dim)
     return output_sig.transpose()
-
-    
-def hp_parameters():
-    """
-    Gives the linear matrices and nonlinear operators of the state-space
-    representation for the loudspeaker SICA Z000900
-    (http://www.sica.it/media/Z000900C.pdf551d31b7b491e.pdf).
-    """ 
-
-    state_dim = 3
-    input_dim = 1
-    output_dim = 1
-    sym_bool = True
-    
-    ## Linear part ##
-
-    # Electric parameters
-    Bl = 2.99 # Electodynamic driving parameter [T.m]
-    Re = 5.7 # Electrical resistance of voice coil   [Ohm]
-    Le  =   0.11e-3 # Coil inductance [H]
-    # Mechanical parameters
-    Mms = 1.9e-3; # Mechanical mass [kg]
-    Cms = 544e-6; # Mechanical compliance [m.N-1]
-    Qms = 4.6;
-    k = 1 / Cms # Suspension stiffness [N.m-1]
-    Rms = np.sqrt(k * Mms)/Qms; # Mechanical damping and drag force [kg.s-1]   
-    # State-space matrices
-    A_m = np.array([[-Re/Le, 0, -Bl/Le],
-                    [0, 0, 1],
-                    [Bl/Mms, -k/Mms, -Rms/Mms]]) # State-to-state matrix
-    B_m = np.array([1/Le, 0, 0]); # Input-to-state matrix
-    C_m = np.array([[1, 0, 0]]) # State-to-output matrix  
-    D_m = np.zeros((output_dim, input_dim)) # Input-to-output matrix    
-
-    ## Nonlinear part ##
-    
-    # Suspension stiffness polynomial expansion
-    k_poly_coeff_v = np.array([k, -554420.0, 989026000])
-    mpq_coeff = k_poly_coeff_v/Mms
-    # Handles for fonction saying if Mpq and Npq functions are used
-    h_mpq_bool = (lambda p, q: q==0) 
-    h_npq_bool = (lambda p, q: False)
-    # Handles for fonction giving the Mpq tensor
-    h_mpq = (lambda p, q: hp_mpq_tensor(p, q, state_dim,  mpq_coeff[p-1]))
-    h_npq = (lambda p, q: None)  
-
-    def hp_mpq_tensor(p, q, state_dim, coeff_value):
-        """
-        Gives the tensor form of the Mpq function (with q = 0).
-        """ 
-        if q==0:
-            mpq_tensor = np.zeros((state_dim,)*(p+1))
-            idx = np.concatenate((np.array([2], dtype=int),
-                                  np.ones(p, dtype=int)))
-            mpq_tensor[tuple(idx)] = coeff_value
-            return mpq_tensor
-        else:
-            return None
-            
-    return (A_m, B_m, C_m, D_m), (h_mpq_bool, h_mpq), (h_npq_bool, h_npq), \
-    (input_dim, state_dim, output_dim), sym_bool
-
-
-#==============================================================================
-# Main script
-#==============================================================================
-
-if __name__ == '__main__':
-    """
-    Main script for testing.
-    """
-        
-    # Input signal
-    fs = 20000 # Sampling frequency
-    T = 0.5 # Duration 
-    f1 = 160 # Starting fundamental frequency
-    f2 = 160 # Ending fundamental frequency
-    amp = 10 # Amplitude    
-    
-    time_vector = np.arange(0, T, step=1/fs)
-    f0_vector = np.linspace(f1, f2, num=len(time_vector))
-    sig = amp * np.cos(2 * np.pi * f0_vector * time_vector)
-    
-    # Loudspeakers parameters
-    matrices, m_pq, n_pq, sizes, sym_bool = hp_parameters()
-    
-    for ampIdx in np.arange(amp):
-        # Simulation
-        sig = (ampIdx+1) * np.cos(2 * np.pi * f0_vector * time_vector)
-        output = simulation(sig, matrices, m_pq, n_pq, sizes=sizes,
-                        fs=fs, nl_order_max=3, hold_opt=0, plot_opt=True)
-    
-        plt.figure('Input and output {}'.format(max(abs(sig))))
-        plt.clf()
-        plt.subplot(2, 1, 1)
-        plt.plot(time_vector, sig, 'b')
-        plt.xlim([0.00625, 0.01925])
-        plt.subplot(2, 1, 2)
-        plt.plot(time_vector, output, 'r')
-        plt.xlim([0.00625, 0.01925])
-    plt.show()
-    
-    print('Test combinatoire')
-    make_list_pq_set(lambda p, q: True, 4, True)
-    print('\n\nHP')
-    make_list_pq_set(m_pq[0], 4, True)
