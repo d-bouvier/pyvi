f0ba6e5826d5be0d17cb3ef8428527804ce2c6e5
Damien Bouvier <damien.bouvier@ircam.fr>
2016-10-31 12:53:01 +0100

    Add a plot for collection of signals.
    Also change order of arguments
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- utilities/plotbox.py
+++ utilities/plotbox.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 # Functions
 #==============================================================================
 
-def plot_sig_io(input_sig, output_sig, name, time_vec, 
+def plot_sig_io(input_sig, output_sig, time_vec, name=None,
                 xlim=[None, None], ylim=[None, None]):
     complex_bool = 'complex' in str(input_sig.dtype) or \
                    'complex' in str(output_sig.dtype)
@@ -52,6 +52,37 @@ def plot_sig_io(input_sig, output_sig, name, time_vec,
     plt.show()
 
 
+def plot_sig_coll(sig_coll, time_vec, name=None, title_plots=None, 
+                  xlim=[None, None], ylim=[None, None], dim=1):
+    nb_sig = sig_coll.shape[dim]
+    complex_bool = 'complex' in str(sig_coll.dtype)
+    if title_plots is None: 
+        title_plots = ['Signal {}'.format(n+1) for n in range(nb_sig)]
+    
+    plt.figure(name)
+    plt.clf()
+    
+    if complex_bool:
+        for n in range(nb_sig):
+            plt.subplot(nb_sig, 2, 2*n+1)
+            plt.plot(time_vec, sig_coll[:, n].real, 'b')
+            plt.title(title_plots[n] + ' - real part')
+            plt.xlim(xlim)
+            plt.ylim(ylim)
+            plt.subplot(nb_sig, 2, 2*n+2)
+            plt.plot(time_vec, sig_coll[:, n].imag, 'r')
+            plt.title(title_plots[n] + ' - imaginary part')
+            plt.xlim(xlim)
+            plt.ylim(ylim)
+    else:
+        for n in range(nb_sig):
+            plt.subplot(nb_sig, 1, n+1)
+            plt.plot(time_vec, sig_coll[:, n], 'b')
+            plt.title(title_plots[n])
+            plt.xlim(xlim)
+            plt.ylim(ylim)
+    plt.show()
+
 #==============================================================================
 # Main script
 #==============================================================================
@@ -65,10 +96,16 @@ if __name__ == '__main__':
     
     time_vec = np.arange(5, step=1/100)
     sig_1 = np.sin(2 * np.pi * time_vec)
-    sig_2 = np.cos(2 * np.pi * time_vec)
+    sig_2 = np.minimum(sig_1, 0.8)
     sig_3 = np.exp(2j * np.pi * time_vec)
     sig_4 = np.exp(2j * 1.5 * np.pi * time_vec)
     
-    plot_sig_io(sig_1, sig_2, 'Test réel', time_vec, ylim=[-1.1, 1.1])
-    plot_sig_io(sig_3, sig_4, 'Test complexe', time_vec, xlim=[0, 3])
+    plot_sig_io(sig_1, sig_2, time_vec, name='Test réel', ylim=[-1.1, 1.1])
+    plot_sig_io(sig_3, sig_4, time_vec, name='Test complexe', xlim=[0, 3])
+    
+    plot_sig_coll(np.stack((sig_1, sig_2, sig_1 - sig_2), axis=1),
+                  time_vec, name='Test réel (Collection)', ylim=[-1.1, 1.1],
+                  title_plots=['Sinus', 'Cosinus', 'Sinus saturated'])
+    plot_sig_coll(np.stack((sig_3, sig_4), axis=1), time_vec, xlim=[0, 3],
+                  name='Test complexe (Collection)')
     
