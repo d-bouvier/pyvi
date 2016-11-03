fa47877b79ece5489b784411d2f5d3fefe321d5d
Damien Bouvier <damien.bouvier@ircam.fr>
2016-11-03 11:50:41 +0100

    Rearrange statespace.py.
    Modifications:
    - Put definition of class 'System' and system parameters definition functions ('loudspeaker_sica' and 'simple_system') in statespace/statespace.py
    - Rename class 'StateSpace' to 'SymbolicStateSpace' and 'System' to 'StateSpace'
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyVI/simulation/simulation.py
+++ pyVI/simulation/simulation.py
@@ -6,20 +6,6 @@ Set of functions for the numerical simulation of a nonlinear systems given
 its state-space representation.
 
 
-
-Class
------
-System :
-    Defines physical systems by their state-space representations parameters.
-
-System parameters
------------------
-loudspeaker_sica :
-    Returns a System object corresponding to the SICA Z000900 loudspeaker.
-simple_system :
-    Returns a System object corresponding to a simple system for simulation
-    test.
-
 Multilinear combinatorics
 -------------------------
 make_list_pq :
@@ -44,7 +30,7 @@ Notes
 @author:    bouvier@ircam.fr
             Damien Bouvier, IRCAM, Paris
 
-Last modified on 2 Nov. 2016
+Last modified on 3 Nov. 2016
 Developed for Python 3.5.1
 
 """
@@ -55,200 +41,6 @@ Developed for Python 3.5.1
 
 import numpy as np
 from scipy import linalg
-
-
-#==============================================================================
-# Class
-#==============================================================================
-
-class System:
-    """Defines physical systems by their state-space representations parameters.
-
-    Attributes
-    ----------
-    A_m : numpy.ndarray
-        State-to-state matrix
-    B_m : numpy.ndarray
-        Input-to-state matrix
-    C_m : numpy.ndarray
-        State-to-output matrix
-    D_m : numpy.ndarray
-        Input-to-output matrix (feedtrhough matrix)
-    dim : dict
-        Dictionnaries with 3 entries giving respectively the dimension of:
-        - the input
-        - the state
-        - the output
-    is_mpq_used, is_npq_used : function (int, int: boolean)
-        Indicates, for given p & q, if the Mpq and Npq function is used in the
-        system.
-    mpq, npq : dict
-        Store multilinear Mpq and Npq functions, in one of the two following
-        forms:
-        - numpy.ndarray in 'tensor' mode;
-        - function (int, numpy.ndarray, ..., numpy.ndarray: numpy.ndarray) in
-        'function' mode.
-    sym_bool : boolean
-        Indicates if multilinear Mpq and Npq functions are symmetric.
-    mode : {'tensor', 'function'}
-        Define in which mode multilinear Mpq and Npq functions are stored.
-    """
-
-    def __init__(self, A_m, B_m, C_m, D_m,
-                 h_mpq_bool, h_npq_bool, mpq_dict, npq_dict,
-                 sym_bool=False, mode='tensor'):
-        """
-        Initialisation function for System object.
-
-        Parameters
-        ----------
-        A_m : numpy.ndarray
-            State-to-state matrix
-        B_m : numpy.ndarray
-            Input-to-state matrix
-        C_m : numpy.ndarray
-            State-to-output matrix
-        D_m : numpy.ndarray
-            Input-to-output matrix (feedtrhough matrix)
-        h_mpq_bool, npq : function (int, int: boolean)
-            Indicates, for given p & q, if the Mpq and Npq function is used in
-            the system.
-        mpq_dict, npq_dict : dict
-            Store multilinear Mpq and Npq functions, in one of the two following
-            forms:
-            - numpy.ndarray in 'tensor' mode;
-            - function (int, numpy.ndarray, ..., numpy.ndarray: numpy.ndarray)
-            in 'function' mode.
-        sym_bool : boolean, optional
-            Indicates if multilinear Mpq and Npq functions are symmetric.
-        mode : {'tensor', 'function'}, optional
-            Define in which mode multilinear Mpq and Npq functions are stored.
-
-        """
-
-        # Initialize the linear part
-        self.A_m = A_m
-        self.B_m = B_m
-        self.C_m = C_m
-        self.D_m = D_m
-
-        # Extrapolate system dimensions
-        self.dim = {'input': B_m.shape[1],
-                    'state': A_m.shape[0],
-                    'output': C_m.shape[0]}
-
-        # Initialize the nonlinear part
-        self.is_mpq_used = h_mpq_bool
-        self.is_npq_used = h_npq_bool
-        self.mpq = mpq_dict
-        self.npq = npq_dict
-
-        self.sym_bool = sym_bool
-        self.mode = mode
-
-
-#==============================================================================
-# System parameters
-#==============================================================================
-
-def loudspeaker_sica(version='tristan', output='pos', mode='tensor'):
-    """
-    Function that create and returns the System object corresponding to the
-    SICA Z000900 loudspeaker
-    (http://www.sica.it/media/Z000900C.pdf551d31b7b491e.pdf).
-
-    Parameters
-    ----------
-    version : {'tristan', 'CFA'}, optional
-        Version to simulate.
-    output : {'pos', 'current'}, optional
-        Defines the output of the system
-    mode : {'tensor', 'function'}, optional
-        Mode in which are stored Mpq and Npq multilinear functions
-
-    Returns
-    -------
-    Object of class System.
-
-    """
-
-    ## Physical parameters ##
-    # Electric parameters
-    if version == 'tristan': # Electodynamic driving parameter [T.m]
-        Bl = 2.9
-    elif version == 'CFA':
-        Bl = 2.99
-    Re = 5.7 # Electrical resistance of voice coil   [Ohm]
-    Le = 0.11e-3 # Coil inductance [H]
-    # Mechanical parameters
-    Mms = 1.9e-3; # Mechanical mass [kg]
-    if version == 'tristan':
-        Rms = 0.406 # Mechanical damping and drag force [kg.s-1]
-        k = [912.2789, 611.4570, 8e07] # Suspension stiffness [N.m-1]
-    elif version == 'CFA':
-        Cms = 544e-6; # Mechanical compliance [m.N-1]
-        Qms = 4.6;
-        k = [1/Cms, -554420.0, 989026000] # Suspension stiffness [N.m-1]
-        # Mechanical damping and drag force [kg.s-1]
-        Rms = np.sqrt(k[0] * Mms)/Qms;
-
-    # State-space matrices
-    A_m = np.array([[-Re/Le, 0, -Bl/Le],
-                    [0, 0, 1],
-                    [Bl/Mms, -k[0]/Mms, -Rms/Mms]]) # State-to-state matrix
-    B_m = np.array([[1/Le], [0], [0]]); # Input-to-state matrix
-    # State-to-output matrix
-    if output == 'pos':
-        C_m = np.array([[0, 1, 0]])
-    elif output == 'current':
-        C_m = np.array([[1, 0, 0]])
-    D_m = np.zeros((1, 1)) # Input-to-output matrix
-
-    # Handles for fonction saying if Mpq and Npq functions are used
-    h_mpq_bool = (lambda p, q: (p<=3) & (q==0))
-    h_npq_bool = (lambda p, q: False)
-
-     # Dictionnaries of Mpq & Npq tensors
-    if mode == 'tensor':
-        m20 = np.zeros((3, 3, 3))
-        m20[2, 1, 1] = -k[1]/Mms
-        m30 = np.zeros((3, 3, 3, 3))
-        m30[2, 1, 1, 1] = -k[2]/Mms
-    elif mode == 'function':
-        m20 = lambda a, x1, x2: np.stack((np.zeros(a), np.zeros(a), \
-                                    -k[1]/Mms * x1[1] * x2[1]), axis=0)
-        m30 = lambda a, x1, x2, x3: np.stack((np.zeros(a), np.zeros(a), \
-                                    -k[2]/Mms * x1[1] * x2[1] * x3[1]), axis=0)
-
-    mpq_dict = {(2, 0): m20, (3, 0): m30}
-    npq_dict = dict()
-
-    return System(A_m, B_m, C_m, D_m, h_mpq_bool, h_npq_bool,
-                  mpq_dict, npq_dict, sym_bool=True, mode=mode)
-
-def simple_system():
-    """
-    Function that create and returns the System object corresponding to a
-    simple system for simulation test.
-
-    Returns
-    -------
-    Object of class System.
-
-    """
-
-    m20 = np.zeros((2, 2, 2))
-    m20[1, 0, 0] = 1
-    m10 = np.zeros((2, 2, 1))
-    m10[0, 1, 0] = -1
-    m02 = np.zeros((2, 1, 1))
-    m02[0, 0, 0] = 2
-
-    return System(np.array([[-1, 0], [1/2, 1/2]]), np.array([[1], [0]]),
-                  np.array([[1, 0]]), np.zeros((1, 1)),
-                  (lambda p, q: (p+q)<3), (lambda p, q: False),
-                  {(2, 0): m20, (1, 1): m10, (0, 2): m02}, dict(),
-                  sym_bool=True, mode='tensor')
 
 
 #==============================================================================
@@ -624,15 +416,16 @@ if __name__ == '__main__':
     Main script for testing.
     """
 
+    from pyVI.statespace.statespace import loudspeaker_sica
     from matplotlib import pyplot as plt
     import time
-    
+
     # Input signal
     fs = 44100
     T = 2
     f1 = 100
     f2 = 300
-    amp = 10   
+    amp = 10
     time_vector = np.arange(0, T, step=1/fs)
     f0_vector = np.linspace(f1, f2, num=len(time_vector))
     sig = amp * np.sin(np.pi * f0_vector * time_vector)
@@ -647,20 +440,19 @@ if __name__ == '__main__':
     plt.plot(sig)
     plt.subplot(2, 1, 2)
     plt.plot(out_t)
-    
+
     start2 = time.time()
     out_f = simulation(sig, loudspeaker_sica(mode='function'),
-                       fs=fs, nl_order_max=3, hold_opt=1) 
+                       fs=fs, nl_order_max=3, hold_opt=1)
     end2 = time.time()
     plt.figure('Input- Output (2)')
     plt.subplot(2, 1, 1)
     plt.plot(sig)
     plt.subplot(2, 1, 2)
     plt.plot(out_f)
-    
+
     plt.figure('Difference')
     plt.plot(out_t - out_f)
-    
+
     print('"tensor" mode: {}s'.format(end1-start1))
     print('"function" mode: {}s'.format(end2-start2))
-    
