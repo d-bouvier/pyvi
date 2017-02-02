697b8367f1d0823cc471b55f6888a7c1d6ea8bca
Damien Bouvier <damien.bouvier@ircam.fr>
2017-02-02 09:48:04 +0100

    Add 'order_separation' function and minor changes
    Changes:
    - fix nl order in first dimension where necessary (in estimation_measure and for 'out_by_order' in simu_collection)
    - K parameter in boyd method can now be chosen (the separation will use a least-square algorithm)
    - w parameter in complex method is now fixed to the N-th root unit
    - add order_separation function
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyVI/tools/order_separation.py
+++ pyVI/tools/order_separation.py
@@ -29,13 +29,13 @@ def estimation_measure(signals_ref, signals_est, mode='default'):
     the better the estimation is. If the signal and its estimation are equal,
     this function returns - np.Inf.
     """
-    nb_sig = signals_est.shape[1]
+    nb_sig = signals_est.shape[0]
     error_sig = np.abs(signals_ref - signals_est)
     error_measure = []
 
     for n in range(nb_sig):
-        rms_error = rms(error_sig[:,n])
-        rms_ref = rms(signals_ref[:,n])
+        rms_error = rms(error_sig[n])
+        rms_ref = rms(signals_ref[n])
         if mode == 'default':
             val = safe_db(rms_error, rms_ref)
         elif mode == 'normalized':
@@ -73,8 +73,9 @@ def simu_collection(input_sig, system, fs=44100, N=1, hold_opt=1,
 
     def update_parameters():
         if method == 'boyd':
-            param.update({'dtype': 'float64',
-                          'K': param['nl_order_max']})
+            param.update({'dtype': 'float64'})
+            if not 'K' in param:
+                param['K'] = param['nl_order_max']
             if not 'coeff' in param:
                 param['coeff'] = np.zeros((param['K']))
                 for idx in range(param['K']):
@@ -82,8 +83,7 @@ def simu_collection(input_sig, system, fs=44100, N=1, hold_opt=1,
         elif method == 'complex':
             param.update({'dtype': 'complex128',
                           'K': param['nl_order_max']})
-            if not 'w' in param:
-                param['w'] = np.exp(1j * 2 * np.pi / param['K'])
+            param['w'] = np.exp(1j * 2 * np.pi / param['K'])
             if not 'rho' in param:
                 param['rho'] = 1
         return param
@@ -105,7 +105,7 @@ def simu_collection(input_sig, system, fs=44100, N=1, hold_opt=1,
     # Simulation for the basic input
     out_by_order = simulation(input_sig, system, fs=fs, nl_order_max=N,
                               hold_opt=hold_opt, out='output_by_order')
-    out_by_order = out_by_order[:, 0, :]
+    out_by_order = out_by_order[:, 0, :].T
     out_by_order.dtype = param['dtype']
 
     # Initialization
@@ -136,3 +136,23 @@ def simu_collection(input_sig, system, fs=44100, N=1, hold_opt=1,
                      'output_collection': output_coll,
                      'time': [n / fs for n in range(len_sig)]},
                     'data', folders)
+
+    return output_coll, param
+
+
+def order_separation(output_coll, method, param):
+    """
+    Make separation of nonlinear order for a given method.
+    (only works with SISO system)
+    """
+    from scipy import fftpack
+    if method == 'boyd':
+        mixing_mat = np.vander(param['coeff'], N=4, increasing=True)[:, 1::]
+        if mixing_mat.shape[0] == mixing_mat.shape[1]:
+            return np.dot(np.linalg.inv(mixing_mat), output_coll)
+        else:
+            return np.dot(np.linalg.inv(np.dot(mixing_mat.T, mixing_mat)),
+                          np.dot(mixing_mat.T, output_coll))
+    elif method == 'complex':
+        return fftpack.ifft(output_coll, n=param['nl_order_max'],
+                            axis=0)[::-1, :]
