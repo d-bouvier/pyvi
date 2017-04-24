6c49e8f282bed383e485902215c39bcaeb9327ad
Damien Bouvier <damien.bouvier@ircam.fr>
2017-04-24 14:33:46 +0200

    Move files to new architecture.
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

---
+++ pyVI/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration script for pyvi package.
+
+@author: bouvier (bouvier@ircam.fr)
+         Damien Bouvier, IRCAM, Paris
+
+Last modified on 24 Apr. 2017
+Developed for Python 3.6.1
+"""
+
+__author__ = "Damien Bouvier"
+__maintainer__ = "Damien Bouvier"
+__version__ = "0.3"
