4cb6e3ae351179b6cfd973cb7d64379833eb3b81
Damien Bouvier <damien.bouvier@ircam.fr>
2017-04-24 17:13:34 +0200

    Add array_symmetrization function in tools/mathbox.
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyVI/tools/mathbox.py
+++ pyVI/tools/mathbox.py
@@ -35,6 +35,7 @@ def db(val, ref=1):
     """
     return 20 * np.log10(val / ref)
 
+
 def safe_db(num, den):
     """
     Conversion to dB with verification that neither the denominator nor
@@ -46,6 +47,7 @@ def safe_db(num, den):
         return - np.Inf
     return 20 * np.log10(num / den)
 
+
 def binomial(n, k):
     """
     Binomial coefficient returning an integer.
@@ -53,3 +55,33 @@ def binomial(n, k):
 
     from scipy.special import binom as fct_binomial
     return int(fct_binomial(n, k))
+
+
+def array_symmetrization(array):
+    """
+    Symmetrize a multidimensional square array (each dimension must have the
+    same length).
+
+    Parameters
+    ----------
+    array : numpy.ndarray
+        Array to symmetrize.
+
+    Returns
+    -------
+    array_sym : numpy.ndarray
+        Symmetrized array.
+    """
+
+    from math import factorial
+    import itertools as itr
+
+    shape = array.shape
+    assert len(set(shape)) == 1, 'Multidimensional array is not square ' + \
+        '(has shape {})'.format(shape)
+    n = len(array.shape)
+
+    array_sym = np.zeros(shape, dtype=array.dtype)
+    for ind in itr.permutations(range(n), n):
+        array_sym += np.transpose(array, ind)
+    return array_sym / factorial(n)
