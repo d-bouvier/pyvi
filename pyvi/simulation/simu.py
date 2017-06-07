faaf0a2e62ab32fc008ef629e16ac31619950514
Damien Bouvier <damien.bouvier@ircam.fr>
2017-06-07 16:29:40 +0200

    Integrate simulation/kernels.py content into SimulationObject as methods
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyvi/simulation/simu.py
+++ pyvi/simulation/simu.py
@@ -22,8 +22,11 @@ Developed for Python 3.6.1
 
 import numpy as np
 from scipy import linalg
+from scipy.fftpack import fftn
+from itertools import filterfalse, product
 from .combinatorics import make_pq_combinatorics
 from ..system.statespace import NumericalStateSpace
+from ..utilities.mathbox import array_symmetrization
 
 #==============================================================================
 # Class
@@ -232,3 +235,261 @@ class SimulationObject:
             return state_by_order
         if out_opt == 'all':
             return output_by_order, state_by_order
+
+    #=============================================#
+
+    def compute_kernels(self, T, which='both'):
+        """
+        """
+        #TODO docstring
+
+        if which == 'time':
+            return self._compute_time_kernels(T)
+
+        elif which == 'both':
+            volterra_kernels = self._compute_time_kernels(T)
+            transfer_kernels = dict()
+            for n, kernel in volterra_kernels.items():
+                transfer_kernels[n] = np.fft.fftshift(fftn(kernel))
+
+            return volterra_kernels, transfer_kernels
+
+        elif which == 'freq':
+            return self._compute_freq_kernels(T)
+
+    def _compute_time_kernels(self, T):
+        """
+        """
+        #TODO docstring
+
+        ####################
+        ## Initialization ##
+        ####################
+
+        # Input-to-state and input-to-output kernels initialization
+        len_kernels = 1 + int(self.fs * T)
+        kernels_in2state = dict()
+        kernels_in2out = dict()
+        shape = (self.dim['state'],)
+        for n in range(1, self.nl_order_max+1):
+            shape += (len_kernels,)
+            kernels_in2state[n] = np.zeros(shape)
+
+        # Dirac-delta
+        dirac = np.zeros((self.dim['input'], len_kernels))
+        dirac[:, 0] = 1
+
+        # Enforce good shape when input dimension is 1
+        if self.dim['input'] == 1:
+            self.B_m.shape = (self.dim['state'], self.dim['input'])
+            self.D_m.shape = (self.dim['output'], self.dim['input'])
+        if self.dim['output'] == 1:
+            self.C_m.shape = (self.dim['output'], self.dim['state'])
+            self.D_m.shape = (self.dim['output'], self.dim['input'])
+
+        ##################################################
+        ## Creation of functions for kernel computation ##
+        ##################################################
+
+        # Computation of the Mpq/Npq functions (given as tensors)
+        def pq_computation(n, p, q, order_set, pq_tensor):
+            temp_arg = ()
+            min_ind = p + q
+            for count in range(p):
+                max_ind = min_ind + order_set[count]
+                temp_arg += (kernels_in2state[order_set[count]],)
+                temp_arg += ([count] + list(range(min_ind, max_ind)),)
+                min_ind = max_ind
+            for count in range(q):
+                temp_arg += (dirac, [p+count, min_ind+count])
+            temp_arg += (list(range(p + q + n)),)
+            return np.tensordot(pq_tensor, np.einsum(*temp_arg), p+q)
+
+        # Correction of the bias due to ADC converter (with holder of order 0 or 1)
+        if self.holder_order == 0:
+            bias_1sample_lag = self.holder_bias_mat[0]
+            def holder_bias(mpq_output, n):
+                idx_in = [slice(None)] + [slice(len_kernels-1)] * n
+                idx_out = [slice(None)] + [slice(1, len_kernels)] * n
+                kernels_in2state[n][idx_out] += np.tensordot(bias_1sample_lag,
+                                                             mpq_output[idx_in], 1)
+        elif self.holder_order == 1:
+            bias_0sample_lag = self.holder_bias_mat[0] - self.holder_bias_mat[1]
+            bias_1sample_lag = self.holder_bias_mat[1]
+            def holder_bias(mpq_output, n):
+                kernels_in2state[n] += np.tensordot(bias_0sample_lag, mpq_output, 1)
+                idx_in = [slice(None)] + [slice(len_kernels-1)] * n
+                idx_out = [slice(None)] + [slice(1, len_kernels)] * n
+                kernels_in2state[n][idx_out] += np.tensordot(bias_1sample_lag,
+                                                             mpq_output[idx_in], 1)
+
+        # Filter function
+        def filtering(n):
+            for ind in range(n,n*(len_kernels-1)+1):
+                for indexes in filterfalse(lambda x: sum(x)-ind,
+                                           product(range(1, len_kernels),
+                                                   repeat=n)):
+                    idx_in = [slice(None)] + [(m-1) for m in indexes]
+                    idx_out = [slice(None)] + list(indexes)
+                    kernels_in2state[n][idx_out] += \
+                            np.tensordot(self.filter_mat,
+                                         kernels_in2state[n][idx_in], 1)
+
+        ########################
+        ## Kernel computation ##
+        ########################
+
+        ## Dynamical equation ##
+        # Linear term
+        holder_bias(self.B_m.dot(dirac), 1)
+        filtering(1)
+        # Nonlinear terms (due to Mpq functions)
+        for n, elt in sorted(self.mpq_combinatoric.items()):
+            for p, q, order_set, nb in elt:
+                mpq_output = nb * pq_computation(n, p, q, order_set,
+                                                 self.mpq[(p, q)])
+                holder_bias(mpq_output, n)
+            for ind in range(self.dim['state']):
+                kernels_in2state[n][ind] = \
+                            array_symmetrization(kernels_in2state[n][ind])
+            filtering(n)
+
+        ## Output equation ##
+        # Linear and nonlinear terms due to matrix C
+        for n in range(1, self.nl_order_max+1):
+            kernels_in2out[n] = np.tensordot(self.C_m, kernels_in2state[n], 1)
+        # Linear term due to matrix D
+        kernels_in2out[1] += self.D_m.dot(dirac)
+        # Other nonlinear terms (due to Npq functions)
+        for n, elt in sorted(self.npq_combinatoric.items()):
+            for p, q, order_set, nb in elt:
+                kernels_in2out[n] += nb * pq_computation(n, p, q, order_set,
+                                                         self.npq[(p, q)])
+            for ind in range(self.dim['output']):
+                kernels_in2out[n][ind] = \
+                            array_symmetrization(kernels_in2state[n][ind])
+
+        ######################
+        ## Function outputs ##
+        ######################
+
+        if (self.dim['input'] == 1) or (self.dim['output'] == 1):
+            for n in range(1, self.nl_order_max+1):
+                kernels_in2state[n] = np.squeeze(kernels_in2state[n])
+                kernels_in2out[n] = np.squeeze(kernels_in2out[n])
+
+        return kernels_in2out
+
+    def _compute_freq_kernels(self, T):
+        """
+        """
+        #TODO docstring
+
+        ####################
+        ## Initialization ##
+        ####################
+
+        # Frequency vector
+        len_kernels = 1 + int(self.fs * T)
+        freq_vec = np.fft.fftshift(np.fft.fftfreq(len_kernels, d=1/self.fs))
+
+        # Input-to-state and input-to-output kernels initialization
+        kernels_in2state = dict()
+        kernels_in2out = dict()
+        shape = (self.dim['state'],)
+        for n in range(1, self.nl_order_max+1):
+            shape += (len_kernels,)
+            kernels_in2state[n] = np.zeros(shape, dtype=np.complex)
+
+        # Enforce good shape when input dimension is 1
+        if self.dim['input'] == 1:
+            self.B_m.shape = (self.dim['state'], self.dim['input'])
+            self.D_m.shape = (self.dim['output'], self.dim['input'])
+        if self.dim['output'] == 1:
+            self.C_m.shape = (self.dim['output'], self.dim['state'])
+            self.D_m.shape = (self.dim['output'], self.dim['input'])
+
+        # Dirac-delta in frequency domain, with bias due to the sampler holder
+        if self.holder_order == 0:
+            input_freq = np.exp(- 1j*np.pi/self.fs) * np.sinc(freq_vec/self.fs)
+        elif self.holder_order == 1:
+            input_freq = np.sinc(freq_vec/self.fs)**2
+        input_freq = np.reshape(input_freq, (self.dim['input'], len_kernels))
+
+       ##################################################
+        ## Creation of functions for kernel computation ##
+        ##################################################
+
+        # Computation of the Mpq/Npq functions (given as tensors)
+        def pq_computation(n, p, q, order_set, pq_tensor):
+            temp_arg = ()
+            min_ind = p + q
+            for count in range(p):
+                max_ind = min_ind + order_set[count]
+                temp_arg += (kernels_in2state[order_set[count]],)
+                temp_arg += ([count] + list(range(min_ind, max_ind)),)
+                min_ind = max_ind
+            for count in range(q):
+                temp_arg += (input_freq, [p+count, min_ind+count])
+            temp_arg += (list(range(p + q + n)),)
+            return np.tensordot(pq_tensor, np.einsum(*temp_arg), p+q)
+
+        # Filter function
+        def filtering(n):
+            freq_tensor = np.zeros((len_kernels,)*n)
+            freq_tensor += freq_vec
+            idx = [slice(None)]
+            for ind in range(1, n):
+                idx += [None]
+                freq_tensor += freq_vec[idx]
+            s = np.reshape(2j*np.pi*freq_tensor, freq_tensor.shape + (1, 1))
+            identity = np.identity(self.dim['state'])
+            filter_values = np.linalg.inv(s * identity - self.A_m)
+            return np.einsum('...ij,j...->i...', filter_values,
+                             kernels_in2state[n])
+            return np.tensordot(filter_values,  kernels_in2state[n], 1)
+
+        ########################
+        ## Kernel computation ##
+        ########################
+
+        ## Dynamical equation ##
+        # Linear term
+        kernels_in2state[1] = np.dot(self.B_m, input_freq)
+        kernels_in2state[1] = filtering(1)
+        # Nonlinear terms (due to Mpq functions)
+        for n, elt in sorted(self.mpq_combinatoric.items()):
+            for p, q, order_set, nb in elt:
+                mpq_output = nb * pq_computation(n, p, q, order_set,
+                                                 self.mpq[(p, q)])
+                kernels_in2state[n] += mpq_output
+            for ind in range(self.dim['state']):
+                kernels_in2state[n][ind] = \
+                            array_symmetrization(kernels_in2state[n][ind])
+            kernels_in2state[n] = filtering(n)
+
+        ## Output equation ##
+        # Linear and nonlinear terms due to matrix C
+        for n in range(1, self.nl_order_max+1):
+            kernels_in2out[n] = np.tensordot(self.C_m, kernels_in2state[n], 1)
+        # Linear term due to matrix D
+        kernels_in2out[1] += self.D_m.dot(input_freq)
+        # Other nonlinear terms (due to Npq functions)
+        for n, elt in sorted(self.npq_combinatoric.items()):
+            for p, q, order_set, nb in elt:
+                kernels_in2out[n] += nb * pq_computation(n, p, q, order_set,
+                                                         self.npq[(p, q)])
+            for ind in range(self.dim['output']):
+                kernels_in2out[n][ind] = \
+                            array_symmetrization(kernels_in2state[n][ind])
+
+        ######################
+        ## Function outputs ##
+        ######################
+
+        if (self.dim['input'] == 1) or (self.dim['output'] == 1):
+            for n in range(1, self.nl_order_max+1):
+                kernels_in2state[n] = np.squeeze(kernels_in2state[n])
+                kernels_in2out[n] = np.squeeze(kernels_in2out[n])
+
+        return kernels_in2out
