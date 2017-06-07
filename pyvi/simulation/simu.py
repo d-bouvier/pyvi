ed6aa001d8ed081b416ff8d2e9e867aa2ba0e219
Damien Bouvier <damien.bouvier@ircam.fr>
2017-06-07 16:25:20 +0200

    Integrate StateSpaceSImulationParameters into simulation/simu.py
    Modifications:
     - Suppression of simulation/tools.py
     - Deleting content of simulation/simu.py
     - New class SimulationObject in simulation/simu.py
    
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyvi/simulation/simu.py
+++ pyvi/simulation/simu.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Module forthe numerical simulation of a systems given by a NumericalStateSpace.
+Module for numerical simulation of system given its state-space representation.
 
 Function
 --------
@@ -12,7 +12,7 @@ Notes
 @author: bouvier (bouvier@ircam.fr)
          Damien Bouvier, IRCAM, Paris
 
-Last modified on 06 June 2017
+Last modified on 07 June 2017
 Developed for Python 3.6.1
 """
 
@@ -21,156 +21,62 @@ Developed for Python 3.6.1
 #==============================================================================
 
 import numpy as np
-
+from scipy import linalg
+from .combinatorics import make_pq_combinatorics
+from ..system.statespace import NumericalStateSpace
 
 #==============================================================================
-# Functions
+# Class
 #==============================================================================
 
-def simulation(input_sig, dimensions: dict, nl_order_max: int,
-               filter_mat, B_m, C_m, D_m, mpq: dict, npq: dict,
-               mpq_combinatoric: dict, npq_combinatoric: dict,
-               holder_bias_mat: dict, out_opt: str):
+class SimulationObject:
     """
-    Compute the simulation of a nonlinear system for a given input.
-
-    Parameters
-    ----------
-    input_sig : numpy.ndarray
-        Input signal.
-    system : StateSpace
-        Parameters of the system to simulate.
-    fs : int, optional
-        Sampling frequency.
-    nl_order_max : int, optional
-        Maximum order of nonlinearity to take into account.
-    hold_opt : {0, 1}, optional
-        Type of sample-holder of the ADC converter to emulate.
-    out : {'output', 'output_by_order', 'all'}, optional
-        Option to choose the output.
-
-    Returns
-    -------
-    output_sig : numpy.ndarray
-        Output of the system.
-    output_by_order : numpy.ndarray
-        Output of the system, separated in each order of nonlinearity.
-    state_by_order : numpy.ndarray
-        States of the system, separated in each order of nonlinearity.
-
-    In function of the ``out`` option, this function returns:
-        - ``output_sig`` (if ``out`` == 'output')
-        - ``output_by_order`` (if ``out`` == 'output_by_order')
-        - ``state`` (if ``out`` == 'state')
-        - ``output_sig``, ``state_by_order``, and ``output_by_order`` (if \
-        ``out`` == 'all')
-
     """
-    #TODO update docstring
+    #TODO docstring
 
-    ####################
-    ## Initialization ##
-    ####################
+    def __init__(self, system: NumericalStateSpace, fs=44100, nl_order_max=3,
+                 holder_order=1, resampling=False):
+        """
+        """
+        #TODO docstring
 
-    input_sig = input_sig.copy()
-    dtype = input_sig.dtype
+        # Initialize simulation options
+        self.fs = fs
+        self.nl_order_max = nl_order_max
+        self.holder_order = holder_order
+        self.resampling = resampling
 
-    # Enforce good shape when input dimension is 1
-    if dimensions['input'] == 1:
-        sig_len = input_sig.shape[0]
-        B_m.shape = (dimensions['state'], dimensions['input'])
-        D_m.shape = (dimensions['output'], dimensions['input'])
-        input_sig.shape = (dimensions['input'], sig_len)
-    else:
-        sig_len = input_sig.shape[0]
+        # Filter matrix
+        sampling_time = 1/self.fs
+        self.A_inv = linalg.inv(system.A_m)
+        self.filter_mat = linalg.expm(system.A_m * sampling_time)
 
-    # By-order state and output initialization
-    state_by_order = np.zeros((nl_order_max, dimensions['state'], sig_len),
-                              dtype)
-    output_by_order = np.zeros((nl_order_max, dimensions['output'], sig_len),
-                               dtype)
+        # List of Mpq combinations
+        self.mpq_combinatoric = make_pq_combinatorics(system.mpq,
+                                                      self.nl_order_max,
+                                                      system.pq_symmetry)
 
-    ##########################################
-    ## Creation of functions for simulation ##
-    ##########################################
+        # List of Mpq combinations
+        self.npq_combinatoric = make_pq_combinatorics(system.npq,
+                                                      self.nl_order_max,
+                                                      system.pq_symmetry)
 
-    # Computation of the Mpq/Npq functions (given as tensors)
-    def pq_computation(p, q, order_set, pq_tensor):
-        temp_arg = ()
-        for count in range(p):
-            temp_arg += (state_by_order[order_set[count]],)
-            temp_arg += ([count, p+q],)
-        for count in range(q):
-            temp_arg += (input_sig, [p+count, p+q])
-        temp_arg += (list(range(p+q+1)),)
-        return np.tensordot(pq_tensor, np.einsum(*temp_arg), p+q)
+        # Holder bias matrices
+        temp_mat_1 = self.filter_mat - np.identity(system.dim['state'])
+        temp_mat_2 = np.dot(self.A_inv, temp_mat_1)
+        self.holder_bias_mat = dict()
+        self.holder_bias_mat[0] = temp_mat_2.copy()
+        if self.holder_order == 1:
+            self.holder_bias_mat[1] = np.dot(self.A_inv, self.filter_mat) - \
+                            (1/sampling_time) * np.dot(self.A_inv, temp_mat_2)
 
-    # Correction of the bias due to ADC converter (with holder of order 0 or 1)
-    if len(holder_bias_mat) == 1:
-        bias_1sample_lag = holder_bias_mat[0]
-        def holder_bias(mpq_output, n):
-            state_by_order[n-1,:,1::] += np.dot(bias_1sample_lag,
-                                                mpq_output[:,0:-1])
-    elif len(holder_bias_mat) == 2:
-        bias_0sample_lag = holder_bias_mat[0] - holder_bias_mat[1]
-        bias_1sample_lag = holder_bias_mat[1]
-        def holder_bias(mpq_output, n):
-            state_by_order[n-1,:,:] += np.dot(bias_0sample_lag, mpq_output)
-            state_by_order[n-1,:,1::] += np.dot(bias_1sample_lag,
-                                                mpq_output[:,0:-1])
+        # Copy system dimensions, matrices and pq-functions
+        self.dim = system.dim.copy()
 
-    # Filter function (simply a matrix product by 'filter_mat')
-    def filtering(n):
-        for k in np.arange(sig_len-1):
-            state_by_order[n-1,:,k+1] += filter_mat.dot(state_by_order[n-1,:,k])
+        self.A_m = system.A_m.copy()
+        self.B_m = system.B_m.copy()
+        self.C_m = system.C_m.copy()
+        self.D_m = system.D_m.copy()
 
-    ##########################
-    ## Numerical simulation ##
-    ##########################
-
-    ## Dynamical equation ##
-    # Linear state
-    holder_bias(np.dot(B_m, input_sig), 1)
-    filtering(1)
-    # Nonlinear states (due to Mpq functions)
-    for n, elt in sorted(mpq_combinatoric.items()):
-        for p, q, order_set, nb in elt:
-            mpq_output = nb * \
-                    pq_computation(p, q, [m-1 for m in order_set], mpq[(p, q)])
-            holder_bias(mpq_output, n)
-        filtering(n)
-
-    ## Output equation ##
-    # Output term due to matrix D
-    output_by_order[0] += np.dot(D_m, input_sig)
-    # Output terms due to matrix C
-    for n in range(nl_order_max):
-        output_by_order[n] += np.dot(C_m, state_by_order[n])
-    # Other nonlinear output terms (due to Npq functions)
-    for n, elt in sorted(npq_combinatoric.items()):
-        for p, q, order_set, nb in elt:
-            output_by_order[n-1,:,:] += nb * \
-                    pq_computation(p, q, [m-1 for m in order_set], npq[(p, q)])
-
-    ######################
-    ## Function outputs ##
-    ######################
-
-    # Reshaping state (if necessary)
-    if dimensions['state'] == 1:
-        state_by_order = state_by_order[:, 0, :]
-
-    # Reshaping output (if necessary)
-    if dimensions['output'] == 1:
-        output_by_order = output_by_order[:, 0, :]
-
-    if out_opt == 'output':
-        return output_by_order.sum(0)
-    if out_opt == 'output_by_order':
-        return output_by_order
-    if out_opt == 'state':
-        return state_by_order.sum(0)
-    if out_opt == 'state_by_order':
-        return state_by_order
-    if out_opt == 'all':
-        return output_by_order, state_by_order
+        self.mpq = system.mpq.copy()
+        self.npq = system.npq.copy()
