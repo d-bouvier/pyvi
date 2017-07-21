0e7189bedc430dde0ee98705dcdaecded1248927
Damien Bouvier <damien.bouvier@ircam.fr>
2017-07-21 11:34:21 +0200

    Add global_test.py scripts for launching all tests for a given module or the globally of pivy package
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

---
+++ tests/simulation/global_test.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Global test script for pyvi/utilities module.
+
+Notes
+-----
+@author: bouvier (bouvier@ircam.fr)
+         Damien Bouvier, IRCAM, Paris
+
+Last modified on 21 July 2017
+Developed for Python 3.6.1
+"""
+
+#==============================================================================
+# Importations
+#==============================================================================
+
+import os
+import argparse
+
+
+#==============================================================================
+# Main script
+#==============================================================================
+
+if __name__ == '__main__':
+    """
+    Main script for testing.
+    """
+
+    #####################
+    ## Parsing options ##
+    #####################
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ind', '--indentation', type=int, default=0)
+    args = parser.parse_args()
+    indent = args.indentation
+    ss = ' ' * indent
+
+
+    #######################
+    ## Call test scripts ##
+    #######################
+
+    default_path = os.getcwd()
+    test_base_path = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(test_base_path)
+    command = 'python test_{}.py --indentation ' + str(3 + indent)
+    list_files = ['combinatorics', 'simu']
+    for file_name in list_files:
+        print(ss + 'File:', file_name + '.py')
+        os.system(command.format(file_name))
+    os.chdir(default_path)
