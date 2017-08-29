18e7b5ea045ea08185e46207ecde566863319df7
Damien Bouvier <damien.bouvier@ircam.fr>
2017-08-29 18:04:12 +0200

    Move to version 0.3.5.3
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyvi/__config__.py
+++ pyvi/__config__.py
@@ -13,7 +13,7 @@ Developed for Python 3.6.1
 
 __author__ = "Damien Bouvier"
 __maintainer__ = "Damien Bouvier"
-__version__ = "0.3.5.2"
+__version__ = "0.3.5.3"
 __dependencies__ = {'numpy': '1.13.1',
                     'scipy': '0.19.1',
                      'sympy': '1.0',
