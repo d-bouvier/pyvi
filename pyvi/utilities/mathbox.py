e0e4c7f1213b659edf5e57caac11a3f819695b15
Damien Bouvier <Damien.Bouvier@ircam.fr>
2017-11-24 17:29:05 +0100

    Improve safe_db() in utilities/mathbox.py
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/utilities/mathbox.py
+++ pyvi/utilities/mathbox.py
@@ -12,6 +12,8 @@ safe_db :
     Returns the dB value, with safeguards if numerator or denominator is null.
 binomial :
     Binomial coefficient returning an integer.
+multinomial :
+    Multinomial coefficient returning an integer.
 array_symmetrization :
     Symmetrize a multidimensional square array.
 
@@ -20,7 +22,7 @@ Notes
 @author: bouvier (bouvier@ircam.fr)
          Damien Bouvier, IRCAM, Paris
 
-Last modified on 25 Oct. 2017
+Last modified on 24 Nov. 2017
 Developed for Python 3.6.1
 """
 
@@ -107,9 +109,8 @@ def safe_db(num, den):
     else:
         _den = den
 
-    # Assert same shape
-    assert _num.shape == _den.shape, 'Dimensions of num and den not equal' + \
-        ' (they are respectively {} and {}).'.format(_num.shape, _den.shape)
+    # Broadcast arrays
+    _num, _den = np.broadcast_arrays(_num, _den)
 
     if _num.shape == ():
         if _num == 0:
