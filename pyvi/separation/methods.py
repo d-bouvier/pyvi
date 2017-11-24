e9202d65c6be76322c88d0b6b28a769ec78757c9
Damien Bouvier <Damien.Bouvier@ircam.fr>
2017-11-24 11:08:26 +0100

    Add property condition_number in class for separation methods
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/separation/methods.py
+++ pyvi/separation/methods.py
@@ -43,8 +43,6 @@ from ..utilities.mathbox import binomial
 # Class
 #==============================================================================
 
-#TODO add condition number
-
 class _SeparationMethod:
     """
     Base class for order separation methods.
@@ -76,6 +74,7 @@ class _SeparationMethod:
         self.N = N
         self.K = K
         self.factors = factors
+        self.condition_number = []
 
     def gen_inputs(self, signal):
         """
@@ -176,15 +175,15 @@ class AS(_SeparationMethod):
         """
 
         mixing_mat = \
-            np.vander(self.factors, N=self.N+1, increasing=True)[:,1::]
+            np.vander(self.factors, N=self.N+1, increasing=True)[:, 1::]
         return self._inverse_mixing_mat(output_coll, mixing_mat)
 
-    @staticmethod
-    def _inverse_mixing_mat(output_coll, mixing_mat):
+    def _inverse_mixing_mat(self, output_coll, mixing_mat):
         """
         Resolves the vandermonde system via inverse or pseudo-inverse.
         """
 
+        self.condition_number.append(np.linalg.cond(mixing_mat))
         is_square = mixing_mat.shape[0] == mixing_mat.shape[1]
         if is_square: # Square matrix
             return np.dot(np.linalg.inv(mixing_mat), output_coll)
@@ -258,12 +257,12 @@ class _PS(_SeparationMethod):
             demixing_vec = np.vander([1/self.rho], N=self.N, increasing=True)
             return demixing_vec.T * np.roll(estimation, -1, axis=0)
 
-    @staticmethod
-    def _inverse_fft(output_coll, N):
+    def _inverse_fft(self, output_coll, N):
         """
         Invert Discrete Fourier Transform using the FFT algorithm.
         """
 
+        self.condition_number.append(1)
         return sc_fft.ifft(output_coll, n=N, axis=0)
 
 
@@ -330,7 +329,7 @@ class PS(_PS):
         return 2 * np.real(_SeparationMethod.gen_inputs(self, signal))
 
     def process_outputs(self, output_coll):
-        return _PS._inverse_fft(output_coll, self.nb_phase)
+        return _PS._inverse_fft(self, output_coll, self.nb_phase)
 
 
 class PAS(PS, AS):
@@ -422,13 +421,13 @@ class PAS(PS, AS):
             combinatorial_terms = dict()
 
         mixing_mat = \
-            np.vander(self.amp_vec, N=self.N+1, increasing=True)[:,1::]
+            np.vander(self.amp_vec, N=self.N+1, increasing=True)[:, 1::]
 
         # Inverse DFT for each set with same amplitude
         for idx in range(self.nb_amp):
             start = idx * self.nb_phase
             end = start + self.nb_phase
-            out_per_phase[idx] = PS._inverse_fft(output_coll[start:end],
+            out_per_phase[idx] = PS._inverse_fft(self, output_coll[start:end],
                                                  self.nb_phase)
 
         # Computation of indexes and necessary vector
@@ -438,7 +437,7 @@ class PAS(PS, AS):
         # Inverse Vandermonde matrix for each set with same phase
         for phase_idx in range(self.nb_phase):
             col_idx = np.arange(first_nl_order[phase_idx], self.N+1, 2) - 1
-            tmp = AS._inverse_mixing_mat(out_per_phase[:, phase_idx],
+            tmp = AS._inverse_mixing_mat(self, out_per_phase[:, phase_idx],
                                          mixing_mat[:, col_idx])
 
             for ind in range(tmp.shape[0]):
@@ -529,7 +528,7 @@ class PAS_v2(PAS):
 
         factors = []
         for ii, nb_phase in enumerate(self.nb_phase_vec):
-            factors.append(self.amp_vec[ii] * \
+            factors.append(self.amp_vec[ii] *
                            self._gen_phase_factors(nb_phase))
 
         _SeparationMethod.__init__(self, N, np.sum(self.nb_phase_vec),
@@ -590,7 +589,8 @@ class PAS_v2(PAS):
         start = 0
         for nb_phase in self.nb_phase_vec:
             end = start + nb_phase
-            out_per_phase[start:end] = PS._inverse_fft(output_coll[start:end],
+            out_per_phase[start:end] = PS._inverse_fft(self,
+                                                       output_coll[start:end],
                                                        nb_phase)
             start += nb_phase
 
@@ -600,7 +600,7 @@ class PAS_v2(PAS):
             idx2 = list(idx)
             if (self.N % 2) and ((self.K - 3) in idx):
                 idx2.remove(self.K - 3)
-            tmp = AS._inverse_mixing_mat(out_per_phase[idx],
+            tmp = AS._inverse_mixing_mat(self, out_per_phase[idx],
                                          self.mat[idx, :][:, idx2])
 
             for (n, q) in self.list_nq[ind_test]:
