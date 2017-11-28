b84afdd082153c16f1e4d783896ed84344b4a926
Damien Bouvier <Damien.Bouvier@ircam.fr>
2017-11-28 16:08:30 +0100

    Move to version 3.6
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/__config__.py
+++ pyvi/__config__.py
@@ -13,11 +13,15 @@ Developed for Python 3.6.1
 
 __author__ = "Damien Bouvier"
 __maintainer__ = "Damien Bouvier"
-__version__ = "0.3.5.4"
+__version__ = "0.3.6"
 __dependencies__ = {'numpy': '1.13.1',
                     'scipy': '0.19.1',
                     'sympy': '1.0',
                     'matplotlib': '2.0.2'}
 
-#from . import system
-#from . import utilities
+
+from . import utilities
+from . import system
+from . import simulation as simu
+from . import separation as sep
+from . import identification as identif
