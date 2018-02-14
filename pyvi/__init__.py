9fcd07e128be67cf826c22861f81ddc1e70f3e66
Damien Bouvier <Damien.Bouvier@ircam.fr>
2018-02-14 17:43:08 +0100

    Delete 'system' and 'simulation' submodules and move to version 0.4
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/__init__.py
+++ pyvi/__init__.py
@@ -12,7 +12,5 @@ from .__config__ import (__author__, __maintainer__, __version__,
                          __dependencies__)
 
 from . import utilities
-from . import system
-from . import simulation as simu
 from . import separation as sep
 from . import identification as identif
