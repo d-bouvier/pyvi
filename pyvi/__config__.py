0babed5d8a71881b6468cf69b485531be5a8bef8
Damien Bouvier <Damien.Bouvier@ircam.fr>
2018-02-14 17:46:43 +0100

    Delete 'system' and 'simulation' submodules and move to version 0.4
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/__config__.py
+++ pyvi/__config__.py
@@ -10,8 +10,5 @@ Developed for Python 3.6.1
 
 __author__ = "Damien Bouvier"
 __maintainer__ = "Damien Bouvier"
-__version__ = "0.3.9"
+__version__ = "0.4"
 __author_email__ = 'Damien.Bouvier@ircam.fr'
-__dependencies__ = {'numpy': '1.13.1',
-                    'scipy': '0.19.1',
-                    'sympy': '1.0'}
