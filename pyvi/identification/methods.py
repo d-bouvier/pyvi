c4eae2013941d2cdfb8c9107ea15206506db14a2
Damien Bouvier <Damien.Bouvier@ircam.fr>
2018-03-06 17:50:05 +0100

    Clean docstrings in identification/methods.py
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/identification/methods.py
+++ pyvi/identification/methods.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Toolbox for nonlinear system identification.
+Toolbox for Volterra series system identification.
 
-This package creates functions for Volterra kernel identification via
-Least-Squares methods usign QR decomposition.
+This package creates identification methods for Volterra kernels. It relies
+on a matrix representation of the input-to-output relation of a Volterra
+series, and uses linear algebra tools to estimate the kernels coefficients.
 
 Functions
 ---------
+direct_method :
+    Direct kernel identification on the output signal.
+order_method :
+    Separate kernel identification on each nonlinear homogeneous order.
+term_method :
+    Separate kernel identification on each nonlinear combinatorial term.
+iter_method :
+    Recursive kernel identification on homophase signals.
+Following functions are wrappers for the previous ones, with parameters
+'solver' set to 'QR' and 'out_form' to 'sym'.
 KLS :
     Kernel identification via Least-Squares method using a QR decomposition.
 orderKLS :
@@ -57,6 +68,7 @@ def direct_method(input_sig, output_sig, M, N, **kwargs):
     -------
     kernels : dict(int: numpy.ndarray)
         Dictionary of estimated kernels, where each key is the nonlinear order.
+    {}
     """
 
     def required_nb_data_func(list_nb_coeff):
@@ -64,7 +76,7 @@ def direct_method(input_sig, output_sig, M, N, **kwargs):
         return sum(list_nb_coeff)
 
     def core_func(phi_by_order, out_sig, solver, cast_mode):
-        """Core computation of the estimation."""
+        """Core computation of the identification."""
         mat = np.concatenate([val for n, val in sorted(phi_by_order.items())],
                              axis=1)
         kernels_vec = _solver(mat, out_sig, solver)
@@ -95,6 +107,7 @@ def order_method(input_sig, output_by_order, M, N, **kwargs):
     -------
     kernels : dict(int: numpy.ndarray)
         Dictionary of estimated kernels, where each key is the nonlinear order.
+    {}
     """
 
     def required_nb_data_func(list_nb_coeff):
@@ -102,7 +115,7 @@ def order_method(input_sig, output_by_order, M, N, **kwargs):
         return max(list_nb_coeff)
 
     def core_func(phi_by_order, out_by_order, solver, cast_mode):
-        """Core computation of the estimation."""
+        """Core computation of the identification."""
         kernels_vec = dict()
         for n, phi in phi_by_order.items():
             kernels_vec[n] = _solver(phi, out_by_order[n-1], solver)
@@ -120,7 +133,7 @@ def term_method(input_sig, output_by_term, M, N, **kwargs):
     ----------
     input_sig : numpy.ndarray
         Input signal.
-    output_by_term : dict((int, int): numpy.ndarray}
+    output_by_term : dict((int, int): numpy.ndarray
         Nonlinear combinatorial terms of the output signal; each array
         contained in the dictionar should have the same shape as ``input_sig``;
         the dictionary should contains all keys ``(n, q)`` for
@@ -134,6 +147,7 @@ def term_method(input_sig, output_by_term, M, N, **kwargs):
     -------
     kernels : dict(int: numpy.ndarray)
         Dictionary of estimated kernels, where each key is the nonlinear order.
+    {}
     """
 
     def required_nb_data_func(list_nb_coeff):
@@ -141,7 +155,7 @@ def term_method(input_sig, output_by_term, M, N, **kwargs):
         return max(list_nb_coeff / (1+np.arange(1, N+1)//2))
 
     def core_func(phi_by_term, out_by_term, solver, cast_mode):
-        """Core computation of the estimation."""
+        """Core computation of the identification."""
 
         for (n, k) in phi_by_term.keys():
             phi_by_term[n, k] = _complex2real(phi_by_term[n, k],
@@ -187,6 +201,7 @@ def iter_method(input_sig, output_by_phase, M, N, **kwargs):
     -------
     kernels : dict(int: numpy.ndarray)
         Dictionary of estimated kernels, where each key is the nonlinear order.
+    {}
     """
 
     def required_nb_data_func(list_nb_coeff):
@@ -194,7 +209,7 @@ def iter_method(input_sig, output_by_phase, M, N, **kwargs):
         return max(list_nb_coeff)
 
     def core_func(phi_by_term, out_by_phase, solver, cast_mode):
-        """Core computation of the estimation."""
+        """Core computation of the identification."""
 
         kernels_vec = dict()
         for n in range(N, 0, -1):
@@ -224,7 +239,10 @@ def _identification(input_data, output_data, M, N, required_nb_data_func,
     nb_data = input_data.size
     list_nb_coeff = series_nb_coeff(M, N, form='tri', out_by_order=True)
     required_nb_data = required_nb_data_func(list_nb_coeff)
-    _assert_enough_data_samples(nb_data, required_nb_data, M, N)
+    if nb_data < required_nb_data:
+        raise ValueError('Input signal has {} data samples'.format(nb_data) +
+                         ', it should have at least ' +
+                         '{}.'.format(required_nb_data))
 
     # Create dictionary of combinatorial matrix
     if phi is None:
@@ -285,16 +303,63 @@ def iterKLS(input_sig, output_by_phase, M, N, **kwargs):
     return iter_method(input_sig, output_by_phase, M, N, **kwargs)
 
 
-# Complete docstring of wrapper functions
-_wrapper_doc = """
-    This function is only a wrapper kept for convenience. For full
-    documentation and specification of the keyword arguments, refer to
-    `pyvi.identification.{}_method`."""
+#========================================#
+
+kwargs_docstring_common = """
+    Other parameters
+    ----------------
+    solver : {'LS', 'QR'}, optional (default='LS')
+        Method used for solving linear systems; if set to 'LS', a standard
+        Least-Squares estimate is used; if set to 'QR', a QR decomposition of
+        the matrix to invert is used.
+    out_form : {'vec', sym', 'tri'}, optional (default='vec')
+        Form under which the kernels are returned; if set to 'vec', only
+        vectors regrouping the nonzero coefficients of the diagonal form are
+        returned; if set to 'tri' or 'sym', tensors depicting the triangular
+        or symmetric form are returned.
+    projection : None
+        Orthogonal basis unto which the kernel should be projected for
+        identification; unused for the moment."""
+kwargs_docstring_phi_order = """
+    phi : dict(int: numpy.ndarray), optional (default=None)
+        Pre-computed dictionary of the combinatorial matrix for each nonlinear
+        homogeneous order."""
+kwargs_docstring_phi_term = """
+    phi : dict((int, int): numpy.ndarray), optional (default=None)
+        Pre-computed dictionary of the combinatorial matrix for each nonlinear
+        combinatorial term."""
+kwargs_docstring_cast_mode = """
+    cast_mode : {'real', 'imag', 'real-imag'}, optional (default='real-imag')
+        Choose how complex number are casted to real numbers; if set to
+        'real-imag', arrays for the real and imaginary part will be stacked."""
+
+for mode in ('direct', 'order', 'term', 'iter'):
+    method = locals()[mode + '_method']
+    kwargs_docstring = kwargs_docstring_common
+    if mode in {'direct', 'order'}:
+        kwargs_docstring += kwargs_docstring_phi_order
+    elif mode in {'term', 'iter'}:
+        kwargs_docstring += kwargs_docstring_phi_term
+        kwargs_docstring += kwargs_docstring_cast_mode
+    method.__doc__ = method.__doc__.format(kwargs_docstring)
+
+_wrapper_doc_pre = """
+    This function is only a wrapper kept for convenience; refer to
+    `pyvi.identification.{}_method`.
+    """
+_wrapper_doc_post = """
+    See also
+    --------
+    pyvi.identification.{}_method
+    """
 
 for method, mode in zip((KLS, orderKLS, termKLS, iterKLS),
                         ('direct', 'order', 'term', 'iter')):
-    method.__doc__ += _wrapper_doc.format(mode)
+    method.__doc__ += _wrapper_doc_pre.format(mode)
     corresponding_method_doc = locals()[mode + '_method'].__doc__
-    method.__doc__ += '\n'.join(corresponding_method_doc.splitlines()[1:])
+    method.__doc__ += '\n'.join(corresponding_method_doc.splitlines()[2:])
+    method.__doc__ += _wrapper_doc_post.format(mode)
 
-
+del (kwargs_docstring_common, kwargs_docstring_phi_order,
+     kwargs_docstring_phi_term, kwargs_docstring_cast_mode, kwargs_docstring,
+     method, corresponding_method_doc, mode)
