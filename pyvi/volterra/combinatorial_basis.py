8d52c56537bcf27381a6df16565925faff00038b
Damien Bouvier <Damien.Bouvier@ircam.fr>
2018-03-07 10:40:25 +0100

    Create auxiliary functions and inner functions for `volterrra_basis`
    Modifications:
      - add `_phi_post_processing(phi, N, sorted_by)`
      - add `_combinatorial_mat_diag_terms(signal, m):`
      - put `_copy_and_shift_columns` as an inner function
      - add comments
    
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- pyvi/volterra/combinatorial_basis.py
+++ pyvi/volterra/combinatorial_basis.py
@@ -46,6 +46,32 @@ def volterra_basis(signal, N, M, sorted_by='order'):
         combinatorial term.
     """
 
+
+    def _copy_and_shift_columns(n, m, dec):
+        """Create delayed versions of columns by copying and shifting them."""
+
+        slices_list = []
+        for offset in range(1, m):
+            tmp = max_delay[n][0] + offset
+            ind = np.where(tmp < m)[0]
+            nb_ind = len(ind)
+            max_delay[n][offset] = tmp[ind]
+            slices_list.append((slice(offset, None, None),
+                                slice(dec, dec+nb_ind, None),
+                                slice(None, -offset, None), ind))
+            dec += nb_ind
+
+        def _core(tmp_phi):
+            for slices in slices_list:
+                tmp_phi[slices[0], slices[1]] = tmp_phi[slices[2], slices[3]]
+            return tmp_phi
+
+        phi[(n, 0)] = _core(phi[(n, 0)])
+        if sorted_by == 'term':
+            for k in range(1, 1 + n//2):
+                phi[(n, k)] = _core(phi[(n, k)])
+
+    # Initialization
     _M_save = _as_list(M, N)
     signal = signal.copy()
     len_sig = signal.shape[0]
@@ -58,14 +84,15 @@ def volterra_basis(signal, N, M, sorted_by='order'):
     _M = _M[::-1]
 
     phi = dict()
-    phi[(1, 0)] = sc_lin.toeplitz(signal, np.zeros((1, _M[0])))
-
     max_delay = dict()
     for n in range(1, N+1):
         max_delay[n] = dict()
+
+    # First nonlinear order
+    phi[(1, 0)] = _combinatorial_mat_diag_terms(signal, _M[0])
     max_delay[1][0] = np.arange(_M[0])
 
-    # Loop on nonlinear orders
+    # Loop on other nonlinear orders
     for n, m in zip(range(2, N+1), _M[1:]):
         nb_coeff = kernel_nb_coeff(n, m, form='tri')
         delay = np.concatenate(tuple(max_delay[n-1].values()))
@@ -94,18 +121,18 @@ def volterra_basis(signal, N, M, sorted_by='order'):
                     signal.conj() * phi[(n-1, n//2-1)][:, ind])
 
         # Copy of identic values
-        max_delay[n], slices_list = _copy_and_shift_params(max_delay[n], m,
-                                                           dec)
-        # print(n, max_delay[n])
+        _copy_and_shift_columns(n, m, dec)
 
-        phi[(n, 0)] = _copy_and_shift_columns(phi[(n, 0)], slices_list)
-        if sorted_by == 'term':
-            for k in range(1, 1 + n//2):
-                phi[(n, k)] = _copy_and_shift_columns(phi[(n, k)], slices_list)
-
+    # Keep only columns where there is no input delayed more than wanted
     for (n, k), val in phi.items():
         current_max_delay = np.concatenate(tuple(max_delay[n].values()))
         phi[(n, k)] = val[:, np.where(current_max_delay < _M_save[n-1])[0]]
+
+    return _phi_post_processing(phi, N, sorted_by)
+
+
+def _phi_post_processing(phi, N, sorted_by):
+    """Post processing of the dictionary ``phi``."""
 
     if sorted_by == 'term':
         for (n, k) in phi.keys():
@@ -117,24 +144,6 @@ def volterra_basis(signal, N, M, sorted_by='order'):
     return phi
 
 
-def _copy_and_shift_params(max_delay, m, dec):
-
-    slices_list = []
-    for offset in range(1, m):
-        tmp = max_delay[0] + offset
-        ind = np.where(tmp < m)[0]
-        nb_ind = len(ind)
-        max_delay[offset] = tmp[ind]
-        slices_list.append((slice(offset, None, None),
-                            slice(dec, dec+nb_ind, None),
-                            slice(None, -offset, None), ind))
-        dec += nb_ind
-
-    return max_delay, slices_list
-
-
-def _copy_and_shift_columns(tmp_phi, slices_list):
-
-    for slices in slices_list:
-        tmp_phi[slices[0], slices[1]] = tmp_phi[slices[2], slices[3]]
-    return tmp_phi
+def _combinatorial_mat_diag_terms(signal, m):
+    """Part of the combinatorial matrix corresponding to diagonal terms."""
+    return sc_lin.toeplitz(signal, np.zeros((1, m)))
