81a53b8dbe0b7b20481879551f7f19740008af89
Damien Bouvier <Damien.Bouvier@ircam.fr>
2018-03-07 11:06:55 +0100

    Cleaned and refactor unittest in volterra/test_combinatorial_basis.py
    Signed-off-by: Damien Bouvier <Damien.Bouvier@ircam.fr>

--- tests/volterra/test_combinatorial_basis.py
+++ tests/volterra/test_combinatorial_basis.py
@@ -16,124 +16,143 @@ import unittest
 import numpy as np
 from pyvi.volterra.combinatorial_basis import volterra_basis
 from pyvi.volterra.tools import kernel_nb_coeff
+from pyvi.utilities.tools import _as_list
 
 
 #==============================================================================
 # Test Class
 #==============================================================================
 
-class VolterraBasisTest(unittest.TestCase):
+class _AbstractCombinatorialBasisTest():
+
+    L = 30
+    N = 4
+    order_keys = {1, 2, 3, 4}
+    term_keys = {(1, 0), (2, 0), (2, 1), (3, 0), (3, 1), (4, 0), (4, 1),
+                 (4, 2)}
+    M = None
 
     def setUp(self):
-        self.L = 100
-        self.N = 4
-        self.M = 15
-        self.M_2 = [10, 15, 0, 5]
-        sig_r = np.arange(1, self.L+1)
-        sig_c = np.arange(self.L) + 2j * np.arange(self.L)
-        self.order_keys = {1: 0, 2: 0, 3: 0, 4: 0}
-        self.order_r = volterra_basis(sig_r, self.N, self.M, sorted_by='order')
-        self.order_c = volterra_basis(sig_c, self.N, self.M, sorted_by='order')
-        self.order_r_2 = volterra_basis(sig_r, self.N, self.M_2,
-                                        sorted_by='order')
-        self.order_c_2 = volterra_basis(sig_c, self.N, self.M_2,
-                                        sorted_by='order')
-        self.term_keys = {(1, 0): 0, (2, 0): 0, (2, 1): 0, (3, 0): 0,
-                          (3, 1): 0, (4, 0): 0, (4, 1): 0, (4, 2): 0}
-        self.term_r = volterra_basis(sig_r, self.N, self.M, sorted_by='term')
-        self.term_c = volterra_basis(sig_c, self.N, self.M, sorted_by='term')
-        self.term_r_2 = volterra_basis(sig_r, self.N, self.M_2,
-                                       sorted_by='term')
-        self.term_c_2 = volterra_basis(sig_c, self.N, self.M_2,
-                                       sorted_by='term')
+        sig = self.sig_creation()
+        self._M = _as_list(self.M, self.N)
+        self.order = self.compute_basis_func(sig, sorted_by='order')
+        self.term = self.compute_basis_func(sig, sorted_by='term')
 
-    def test_output_type_for_orders(self):
-        for i, value in enumerate([self.order_r, self.order_c, self.order_r_2,
-                                   self.order_c_2]):
+    def compute_basis_func(self, sig, sorted_by):
+        return None
+
+    def sig_creation(self):
+        return np.arange(1, self.L+1)
+
+    def test_output_type(self):
+        for i, value in enumerate([self.order, self.term]):
             with self.subTest(i=i):
                 self.assertIsInstance(value, dict)
 
-    def test_output_type_for_terms(self):
-        for i, value in enumerate([self.term_r, self.term_c, self.term_r_2,
-                                   self.term_c_2]):
-            with self.subTest(i=i):
-                self.assertIsInstance(value, dict)
+    def test_output_keys_for_orders(self):
+        self.assertSetEqual(set(self.order.keys()), self.order_keys)
 
-    def test_output_shape_for_orders(self):
-        for i, value in enumerate([self.order_r, self.order_c, self.order_r_2,
-                                   self.order_c_2]):
-            with self.subTest(i=i):
-                self.assertEqual(value.keys(), self.order_keys.keys())
-
-    def test_output_shape_for_terms(self):
-        for i, value in enumerate([self.term_r, self.term_c, self.term_r_2,
-                                   self.term_c_2]):
-            with self.subTest(i=i):
-                self.assertEqual(value.keys(), self.term_keys.keys())
+    def test_output_keys_for_terms(self):
+        self.assertSetEqual(set(self.term.keys()), self.term_keys)
 
     def test_basis_shapes_for_orders(self):
-        for i, value in enumerate([self.order_r, self.order_c]):
-            for n, basis in value.items():
-                with self.subTest(i=(i, n)):
-                    nb_coeff = kernel_nb_coeff(n, self.M, form='sym')
-                    self.assertEqual(basis.shape, (self.L, nb_coeff))
-
-    def test_basis_shapes_for_orders_2(self):
-        for i, value in enumerate([self.order_r_2, self.order_c_2]):
-            for n, basis in value.items():
-                with self.subTest(i=(i, n)):
-                    nb_coeff = kernel_nb_coeff(n, self.M_2[n-1], form='sym')
-                    self.assertEqual(basis.shape, (self.L, nb_coeff))
+        for n, basis in self.order.items():
+            with self.subTest(i=n):
+                nb_coeff = kernel_nb_coeff(n, self._M[n-1], form='tri')
+                self.assertEqual(basis.shape, (self.L, nb_coeff))
 
     def test_basis_shapes_for_terms(self):
-        for i, value in enumerate([self.term_r, self.term_c]):
-            for (n, q), basis in value.items():
-                with self.subTest(i=(i, (n, q))):
-                    nb_coeff = kernel_nb_coeff(n, self.M, form='sym')
-                    self.assertEqual(basis.shape, (self.L, nb_coeff))
+        for (n, k), basis in self.term.items():
+            with self.subTest(i=(n, k)):
+                nb_coeff = kernel_nb_coeff(n, self._M[n-1], form='tri')
+                self.assertEqual(basis.shape, (self.L, nb_coeff))
 
-    def test_basis_shapes_for_terms_2(self):
-        for i, value in enumerate([self.term_r_2, self.term_c_2]):
-            for (n, q), basis in value.items():
-                with self.subTest(i=(i, (n, q))):
-                    nb_coeff = kernel_nb_coeff(n, self.M_2[n-1], form='sym')
-                    self.assertEqual(basis.shape, (self.L, nb_coeff))
-
-    def test_same_result_with_term_and_order_on_real_signals(self):
+    def test_same_result_with_term_and_order(self):
         for n in range(1, self.N+1):
             with self.subTest(i=n):
-                self.assertTrue(np.all(self.order_r[n] == self.term_r[(n, 0)]))
-
-    def test_same_result_with_term_and_order_on_real_signals_2(self):
-        for n in range(1, self.N+1):
-            with self.subTest(i=n):
-                self.assertTrue(np.all(self.order_r_2[n] ==
-                                       self.term_r_2[(n, 0)]))
-
-    def test_same_result_with_term_and_order_on_complex_signals(self):
-        for n in range(1, self.N+1):
-            with self.subTest(i=n):
-                self.assertTrue(np.all(self.order_c[n] == self.term_c[(n, 0)]))
-
-    def test_same_result_with_term_and_order_on_complex_signals_2(self):
-        for n in range(1, self.N+1):
-            with self.subTest(i=n):
-                self.assertTrue(np.all(self.order_c_2[n] ==
-                                       self.term_c_2[(n, 0)]))
+                self.assertTrue(np.all(self.order[n] == self.term[(n, 0)]))
 
     def test_same_result_between_all_terms_with_real_signals(self):
         for n in range(1, self.N+1):
-            term = self.term_r[(n, 0)]
-            for q in range(1, 1+n//2):
-                with self.subTest(i=(n, q)):
-                    self.assertTrue(np.all(term == self.term_r[(n, q)]))
+            term = self.term[(n, 0)]
+            for k in range(1, 1+n//2):
+                with self.subTest(i=(n, k)):
+                    self.assertTrue(np.all(term == self.term[(n, k)]))
 
-    def test_same_result_between_all_terms_with_real_signals_2(self):
-        for n in range(1, self.N+1):
-            term = self.term_r_2[(n, 0)]
-            for q in range(1, 1+n//2):
-                with self.subTest(i=(n, q)):
-                    self.assertTrue(np.all(term == self.term_r_2[(n, q)]))
+
+class VolterraBasisTest(_AbstractCombinatorialBasisTest, unittest.TestCase):
+
+    M = 15
+
+    def compute_basis_func(self, sig, sorted_by):
+        return volterra_basis(sig, self.N, self.M, sorted_by=sorted_by)
+
+
+class VolterraBasis_M_List_Test(VolterraBasisTest):
+
+    M = [10, 15, 0, 5]
+
+
+class VolterraBasisCplxSigTest(VolterraBasis_M_List_Test):
+
+    test_same_result_between_all_terms_with_real_signals = property()
+
+    def sig_creation(self):
+        return super().sig_creation() + 1j * super().sig_creation()
+
+
+class VolterraBasisCorrectOutputTest(VolterraBasisCplxSigTest):
+
+    L = 4
+    N = 3
+    M = 3
+    true = {(1, 0): np.array([[1+1j, 2+2j, 3+3j, 4+4j],
+                              [  0., 1+1j, 2+2j, 3+3j],
+                              [  0.,   0., 1+1j, 2+2j]]).T,
+            (2, 0): np.array([[2j, 8j, 18j, 32j],
+                              [0., 4j, 12j, 24j],
+                              [0.,  0,  6j, 16j],
+                              [0., 2j,  8j, 18j],
+                              [0., 0.,  4j, 12j],
+                              [0., 0.,  2j,  8j]]).T,
+            (2, 1): np.array([[2., 8., 18., 32.],
+                              [0., 4., 12., 24.],
+                              [0.,  0,  6., 16.],
+                              [0., 2.,  8., 18.],
+                              [0., 0.,  4., 12.],
+                              [0., 0.,  2.,  8.]]).T,
+            (3, 0): np.array([[-2+2j, -16+16j, -54+54j, -128+128j],
+                              [   0.,   -8+8j, -36+36j,   -96+96j],
+                              [   0.,      0., -18+18j,   -64+64j],
+                              [   0.,   -4+4j, -24+24j,   -72+72j],
+                              [   0.,      0., -12+12j,   -48+48j],
+                              [   0.,      0.,   -6+6j,   -32+32j],
+                              [   0.,   -2+2j, -16+16j,   -54+54j],
+                              [   0.,      0.,   -8+8j,   -36+36j],
+                              [   0.,      0.,   -4+4j,   -24+24j],
+                              [   0.,      0.,   -2+2j,   -16+16j]]).T,
+            (3, 1): np.array([[2+2j, 16+16j, 54+54j, 128+128j],
+                              [  0.,   8+8j, 36+36j,   96+96j],
+                              [  0.,     0., 18+18j,   64+64j],
+                              [  0.,   4+4j, 24+24j,   72+72j],
+                              [  0.,     0., 12+12j,   48+48j],
+                              [  0.,     0.,   6+6j,   32+32j],
+                              [  0.,   2+2j, 16+16j,   54+54j],
+                              [  0.,     0.,   8+8j,   36+36j],
+                              [  0.,     0.,   4+4j,   24+24j],
+                              [  0.,     0.,   2+2j,   16+16j]]).T}
+    order_keys = {1, 2, 3}
+    term_keys = {(1, 0), (2, 0), (2, 1), (3, 0), (3, 1)}
+
+    def test_correct_output_order(self):
+        for n, val in self.order.items():
+            with self.subTest(i=n):
+                self.assertTrue(np.all(val == self.true[(n, 0)]))
+
+    def test_correct_output_term(self):
+        for (n, k), val in self.term.items():
+            with self.subTest(i=(n, k)):
+                self.assertTrue(np.all(val == self.true[(n, k)]))
 
 
 #==============================================================================
