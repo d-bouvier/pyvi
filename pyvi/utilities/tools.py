e870187208ace9eb3e40d0a5b9847364bcc6ea92
Damien Bouvier <damien.bouvier@ircam.fr>
2018-06-28 12:06:52 +0200

    Change functionality of DocInherit for debugging problems in case of complex class inheritance.
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- pyvi/utilities/tools.py
+++ pyvi/utilities/tools.py
@@ -51,31 +51,20 @@ class DocInherit(object):
         self.name = mthd.__name__
 
     def __get__(self, obj, cls):
-        if obj:
-            return self.get_with_inst(obj, cls)
-        else:
-            return self.get_no_inst(cls)
-
-    def get_with_inst(self, obj, cls):
-
-        overridden = getattr(super(cls, obj), self.name, None)
-
-        @wraps(self.mthd, assigned=('__name__', '__module__'))
-        def f(*args, **kwargs):
-            return self.mthd(obj, *args, **kwargs)
-
-        return self.use_parent_doc(f, overridden)
-
-    def get_no_inst(self, cls):
 
         for parent in cls.__mro__[1:]:
             overridden = getattr(parent, self.name, None)
             if overridden:
                 break
 
-        @wraps(self.mthd, assigned=('__name__', ' __module__'))
-        def f(*args, **kwargs):
-            return self.mthd(*args, **kwargs)
+        if obj:
+            @wraps(self.mthd, assigned=('__name__', '__module__'))
+            def f(*args, **kwargs):
+                return self.mthd(obj, *args, **kwargs)
+        else:
+            @wraps(self.mthd, assigned=('__name__', ' __module__'))
+            def f(*args, **kwargs):
+                return self.mthd(*args, **kwargs)
 
         return self.use_parent_doc(f, overridden)
 
