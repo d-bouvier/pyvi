08cdf6a98e0df7455b0096e8d4586bd6a6082a10
Damien Bouvier <damien.bouvier@ircam.fr>
2018-08-10 10:55:24 +0200

    Suppress test for `should_be_absent_properties` in test_imports.py
    Signed-off-by: Damien Bouvier <damien.bouvier@ircam.fr>

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -26,17 +26,11 @@ class PyviTestCase(unittest.TestCase):
     needed_properties = ['__author__', '__maintainer__', '__version__',
                          'utilities', 'volterra', 'separation',
                          'identification']
-    should_be_absent_properties = []
 
     def test_should_be_present_properties(self):
         for property_name in self.needed_properties:
             with self.subTest(i=property_name):
                 self.assertTrue(hasattr(self.module, property_name))
-
-    def test_should_be_absent_properties(self):
-        for property_name in self.should_be_absent_properties:
-            with self.subTest(i=property_name):
-                self.assertFalse(hasattr(self.module, property_name))
 
 
 class UtilitiesTestCase(PyviTestCase):
@@ -47,8 +41,6 @@ class UtilitiesTestCase(PyviTestCase):
                          'identification_error', 'LaguerreBasis', 'KautzBasis',
                          'GeneralizedBasis', 'create_orthogonal_basis',
                          'is_valid_basis_instance']
-    should_be_absent_properties = ['_AbstractOrthogonalBasis',
-                                   'inherint_docstring', '_as_list']
 
 
 class VolterraTestCase(PyviTestCase):
@@ -57,18 +49,12 @@ class VolterraTestCase(PyviTestCase):
     needed_properties = ['kernel_nb_coeff', 'series_nb_coeff', 'vec2kernel',
                          'vec2series', 'kernel2vec',
                          'compute_combinatorial_basis']
-    should_be_absent_properties = ['_vec2dict_of_vec', '_check_parameters',
-                                   '_compute_list_nb_coeff',
-                                   '_phi_by_order_post_processing',
-                                   '_combinatorial_mat_diag_terms']
 
 
 class SeparationTestCase(PyviTestCase):
 
     module = pyvi.separation
     needed_properties = ['AS', 'CPS', 'HPS', 'PS', 'PAS']
-    should_be_absent_properties = ['_SeparationMethod', '_AbstractPS',
-                                   '_create_vandermonde_mixing_mat']
 
 
 
@@ -77,9 +63,6 @@ class IdentificationTestCase(PyviTestCase):
     module = pyvi.identification
     needed_properties = ['direct_method', 'order_method', 'term_method',
                          'iter_method', 'phase_method']
-    should_be_absent_properties = ['_solver', '_ls_solver', '_qr_solver',
-                                   '_complex2real', '_identification',
-                                   '_cast_complex2real', '_kwargs_for_KLS']
 
 
 #==============================================================================
